ef964352f2255537c2f8054cb23fedc9a9c7b43a
Micah Elizabeth Scott <micah@misc.name>
2000-06-03 07:30:35 +0000

    New test code for the _extremely buggy_ popup support.

--- cli_perl/tbtest.pl
+++ cli_perl/tbtest.pl
@@ -14,5 +14,20 @@ NewWidget(-type => button, -onclick => \&tuxclick,
 EventLoop;
 
 sub tuxclick {
-    printf "Hello World!\n";
+
+    $w = 150;
+    $h = 60;
+    $s = "Hello, World!\nYou clicked tux.";
+
+    $p = NewPopup($ServerInfo{'Width'}/2-$w/2,$ServerInfo{'Height'}/2-$h/2,$w,$h);
+    NewWidget(-type => label, -text => NewString("Message Box"),
+	      -font => NewFont("",0,bold),-color => 0xFFFFFF,-bgcolor => 0x000000);
+    $tb = NewWidget(-type => toolbar, -side => left);
+    NewWidget(-type => label, -text => NewString($s),-align => nw,
+	      -transparent => 1);
+    
+    NewWidget(-type => button, -onclick => sub {$p->delete},
+	      -inside => $tb, -side => top,
+	      -bitmap => NewBitmap(-file=>'../images/button/check.pnm'),
+	      -bitmask => NewBitmap(-file=>'../images/button/check_mask.pnm'));
 }
