36a30e952439ffdf594500d810e79839fa45134d
Micah Elizabeth Scott <micah@misc.name>
2000-06-09 23:11:56 +0000

    New test that brings out a few bugs in the redraw/recalc code, and demonstrates the cool new text and bitmap buttons.

--- cli_perl/btntest.pl
+++ cli_perl/btntest.pl
@@ -7,12 +7,24 @@ NewWidget(-type => button,
 	  -bitmap => NewBitmap(-file=>'../images/button/tux.pnm'),
 	  -bitmask => NewBitmap(-file=>'../images/button/tux_mask.pnm'));
 
-NewWidget(-type => button,-text => $hi = NewString("X"));
+$a = NewString("X");
+$b = NewString("Hello, World!");
+$w=NewWidget(-type => button,-text => $a,-onclick => sub {
+	if ($i) {
+		$w->SetWidget(-text => $a);
+		$i = 0;
+	}
+	else {
+		$w->SetWidget(-text => $b);
+		$i = 1;
+	}
+	Update();
+});
 
-NewWidget(-type => button,-text => $hi,-font => 
+NewWidget(-type => button,-text => $a,-font => 
 	  NewFont("",10,italic,underline),-color => 0x006666);
 
-NewWidget(-type => button,-text => $hi,-font=>NewFont("Utopia"),
+NewWidget(-type => button,-text => $a,-font=>NewFont("Utopia"),
 	  -bitmap => NewBitmap(-file=>'../images/button/x.pnm'),
 	  -bitmask => NewBitmap(-file=>'../images/button/x_mask.pnm'));
 
