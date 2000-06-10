2009959ba3e1d95a843f035e515f426f127bb6e5
Micah Elizabeth Scott <micah@misc.name>
2000-06-10 00:39:01 +0000

    Another neat test.

--- cli_perl/btntest.pl
+++ cli_perl/btntest.pl
@@ -3,10 +3,6 @@ use PicoGUI;
 
 RegisterApp(-name => NewString("Button test"),-type => toolbar);
 
-NewWidget(-type => button,
-	  -bitmap => NewBitmap(-file=>'../images/button/tux.pnm'),
-	  -bitmask => NewBitmap(-file=>'../images/button/tux_mask.pnm'));
-
 $a = NewString("X");
 $b = NewString("Hello, World!");
 $w=NewWidget(-type => button,-text => $a,-onclick => sub {
@@ -19,18 +15,13 @@ $w=NewWidget(-type => button,-text => $a,-onclick => sub {
 		$i = 1;
 	}
 	Update();
-});
+},
+	     -bitmap => NewBitmap(-file=>'../images/button/tux.pnm'),
+	     -bitmask => NewBitmap(-file=>'../images/button/tux_mask.pnm'));
 
-NewWidget(-type => button,-text => $a,-font => 
-	  NewFont("",10,italic,underline),-color => 0x006666);
 
-NewWidget(-type => button,-text => $a,-font=>NewFont("Utopia"),
-	  -bitmap => NewBitmap(-file=>'../images/button/x.pnm'),
-	  -bitmask => NewBitmap(-file=>'../images/button/x_mask.pnm'));
-
-NewWidget(-type => button,-text => 
-	NewString("This is a test..."),
-	  -bitmap => NewBitmap(-file=>'../images/button/x.pnm'),
-	  -bitmask => NewBitmap(-file=>'../images/button/x_mask.pnm'));
+NewWidget(-type => label,-text => NewString("This is only a test..."),-font => 
+	  NewFont("",10,italic,underline),-color => 0x006666,-transparent=>1,
+	  -side=>left);
 
 EventLoop;
