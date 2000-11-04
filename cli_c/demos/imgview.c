700261a76da0c24cc4dfb713be6e918b4d84a639
Micah Elizabeth Scott <micah@misc.name>
2000-11-04 19:42:43 +0000

    Added a simple image viewer demo

---
+++ cli_c/demos/imgview.c
@@ -0,0 +1,28 @@
+/* little image viewer demo */
+
+#include <picogui.h>
+
+int main(int argc, char *argv[])
+{
+
+  pgInit(argc,argv);
+
+  pgRegisterApp(PG_APP_NORMAL,"Image Viewer",
+		PG_APPSPEC_HEIGHT,200,
+		0);
+
+  pgNewWidget(PG_WIDGET_BITMAP,0,0);
+  pgSetWidget(0,
+	      PG_WP_SIDE,PG_S_ALL,
+	      PG_WP_BITMAP,pgNewBitmap(pgFromFile(argv[2])),
+	      0);
+
+  pgEventLoop();
+  return 0;
+}
+
+
+
+
+
+/* The End */
