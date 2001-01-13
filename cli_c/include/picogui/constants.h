35ac70082e6082cb91d3bc7da344744156488d15
Micah Elizabeth Scott <micah@misc.name>
2001-01-13 06:19:55 +0000

    Updated header for PG_WE_RESIZE

--- cli_c/include/picogui/constants.h
+++ cli_c/include/picogui/constants.h
@@ -1,4 +1,4 @@
-/* $Id: constants.h,v 1.22 2001/01/05 06:41:39 micahjd Exp $
+/* $Id: constants.h,v 1.23 2001/01/13 06:19:55 micahjd Exp $
  *
  * picogui/constants.h - various constants needed by client, server,
  *                       and application
@@ -381,6 +381,7 @@ typedef unsigned long pghandle;
 
 /* Video mode flags */
 #define PG_VID_FULLSCREEN     0x0001
+#define PG_VID_DOUBLEBUFFER   0x0002
 
 /* Logical operations for blits */
 #define PG_LGOP_NULL        0   /* Don't blit */
@@ -477,6 +478,8 @@ typedef unsigned long pghandle;
 #define PG_WE_PNTR_DOWN   4     /* The "mouse" button is now down */
 #define PG_WE_PNTR_UP     5     /* The "mouse" button is now up */
 #define PG_WE_DATA        6     /* Widget is streaming data to the app */
+#define PG_WE_RESIZE      7     /* For terminal widgets, passes size in chars packed
+				 * 16 bits for width and 16 for height: 0xWWWWHHHH */
 
 /* Non-widget events */
 #define PG_NWE_KBD_CHAR    10   /* These are sent if the client has captured the */
@@ -485,7 +488,7 @@ typedef unsigned long pghandle;
 #define PG_NWE_PNTR_MOVE   13
 #define PG_NWE_PNTR_UP     14
 #define PG_NWE_PNTR_DOWN   15
-
+#define PG_NWE_BGCLICK     16   /* The user clicked the background widget */
 
 #endif /* __H_PG_CONSTANTS */
 /* The End */
