56b5653b1586d00ebfa34057c0b13a1b58c48bf8
pney <pney>
2001-06-01 10:43:36 +0000

    in XCopilot environments, env variables must not override default values

--- pgserver/input/tsinput.c
+++ pgserver/input/tsinput.c
@@ -1,4 +1,4 @@
-/* $Id: tsinput.c,v 1.11 2001/05/29 10:35:50 pney Exp $
+/* $Id: tsinput.c,v 1.12 2001/06/01 10:43:36 pney Exp $
  *
  * tsinput.c - input driver for touch screen
  *
@@ -47,7 +47,7 @@
  * timeout for pointing display, backlight (not yet implemented)
  * and sleep mode
  */
-#define POINTING_IDLE_MAX_SEC      2
+#define POINTING_IDLE_MAX_SEC      1
 #define SLEEP_IDLE_MAX_SEC       100
 //#define BACKLIGHT_IDLE_MAX_SEC    50
 
@@ -232,17 +232,17 @@ g_error tsinput_init(void) {
     my1 = 3350; uy1 =   0;
     mx2 =  440; ux2 = 240;
     my2 =  710; uy2 = 320;
-#endif
 
-    /* env var will override default values */
+    /* env var will override default values (but only in ChipSlice!!) */
     if( pg_ts_env = (char*)getenv(PG_TS_ENV_NAME) ) {
       sscanf(pg_ts_env, "%d %d %d %d", &mx1, &my1, &mx2, &my2);
-#ifdef DEBUG_INIT
+#  ifdef DEBUG_INIT
       printf("%s: taking m1 and m2 points for env var: '%s'\n",
 	     _file_, pg_ts_env);
       printf("  mx1=%d my1=%d mx2=%d my2=%d\n", mx1, my1, mx2, my2);
-#endif
+#  endif
     }
+#endif
 
     ts_params.x_ratio_num    = ux1 - ux2;
     ts_params.x_ratio_den    = mx1 - mx2;
