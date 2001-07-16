a33e8cb7bdf0ce39141ca9c377df594b5201e65a
gobry <gobry>
2001-07-16 07:02:10 +0000

    workaround for SDL-less installations

--- pgserver/autogen.sh
+++ pgserver/autogen.sh
@@ -24,6 +24,15 @@ run ()
     $* || error "while running $*"
 }
 
+# check SDL installation
+dir=`aclocal --print-ac-dir`
+if [ -f "${dir}/sdl.m4" ] ; then
+  echo "autogen.sh: using default SDL m4 file"
+else
+  echo "autogen.sh: using private SDL m4 file"
+  aclocal_extra="${aclocal_extra} -I macros/extra"
+fi
+
 run aclocal ${aclocal_extra}
 run autoheader
 run autoconf
