8764e968d78072c304d3abb0b44440d7f399d6eb
Micah Elizabeth Scott <micah@misc.name>
2001-08-01 22:57:31 +0000

    The clock shouldn't ignore errors. This was a workaround for the handle bug.

--- apps/lcdclock/lcdclock.c
+++ apps/lcdclock/lcdclock.c
@@ -1,4 +1,4 @@
-/* $Id: lcdclock.c,v 1.2 2001/07/12 08:10:03 micahjd Exp $
+/* $Id: lcdclock.c,v 1.3 2001/08/01 22:57:31 micahjd Exp $
  * 
  * lcdclock.c - Clock application designed for use with a wall-mounted
  *              LCD (see README) but maybe with other uses
@@ -31,10 +31,6 @@
 
 pghandle wBigClock,wSmallClock;
 
-/* Ignore errors (we'll do anything but interrupt the clock!) */
-void ignore_errs(unsigned short type, const char *msg) {
-}
-
 void sysIdle(void) {
    time_t now;
    struct tm *ltime;
@@ -56,7 +52,6 @@ void sysIdle(void) {
 
 int main(int argc, char **argv) {
    pgInit(argc,argv);
-   pgSetErrorHandler(&ignore_errs);
    pgRegisterApp(PG_APP_NORMAL,"Clock",0);
    pgSetWidget(PGDEFAULT,
 	       PG_WP_SIDE,PG_S_LEFT,
