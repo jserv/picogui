09b792dd74431ca8ea053fd06f4838e51e0eb2dc
Micah Elizabeth Scott <micah@misc.name>
2001-08-10 11:52:10 +0000

    PGL Clock applet is partially functional. Need to commit this to transfer it to my desktop computer for more debugging.

--- apps/pgl/applets/pgl-clock.c
+++ apps/pgl/applets/pgl-clock.c
@@ -1,4 +1,4 @@
-/* $Id: pgl-clock.c,v 1.1 2001/08/09 19:10:23 micahjd Exp $
+/* $Id: pgl-clock.c,v 1.2 2001/08/10 11:52:10 micahjd Exp $
  * 
  * pgl-clock.c - This is a simple clock applet for PGL
  *
@@ -27,19 +27,254 @@
 
 #include <picogui.h>
 #include <time.h>
+#include <string.h>
 
-pghandle wClock;
+/* Maximum length of clock format strings */
+#define FMTMAX 30
 
-/* Update the clock */
-void sysIdle(void) {
+/* Maximum length of clock contents */
+#define CLKMAX 50
+
+/* Clock settings */
+struct clockData {
+  /* The clock itself */
+  pghandle wClock;
+
+  /* Settings */
+  pghandle fClockFont;
+  unsigned int flashColon : 1;
+  unsigned int enable24hour : 1;
+  unsigned int enableSeconds : 1;
+  unsigned int enableWeekDay : 1;
+  unsigned int enableDay : 1;
+  unsigned int enableMonth : 1;
+  unsigned int enableYear : 1;
+
+  /* Format strings- these are generated from the above settings
+   * by the mungeSettings function
+   */
+  char fmt1[40];
+  char fmt2[40];
+
+} currentClock;
+
+/* Functions */
+int btnDialog(struct pgEvent *btnevt);
+void mungeSettings(void);
+void updateTime(void);
+
+/* Options Dialog */
+int btnDialog(struct pgEvent *btnevt) {
+  struct pgEvent evt;
+  pghandle wSampleTB,wOk,wCancel,wSetFont,wOptionBox,wTB;
+  pghandle w24hour,wSeconds,wColon,wWeekDay,wDay,wMonth,wYear;
+  struct clockData oldData;
+
+  /* Save data in case of cancel */
+  oldData = currentClock;
+
+  /* Top-level widgets */
+
+  pgEnterContext();
+  pgDialogBox("Clock Preferences");
+
+  wTB = pgNewWidget(PG_WIDGET_TOOLBAR,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_SIDE,PG_S_BOTTOM,
+	      0);
+
+  wSampleTB = pgNewWidget(PG_WIDGET_TOOLBAR,0,0);
+
+  wOptionBox = pgNewWidget(PG_WIDGET_BOX,0,0);
+
+  pgNewWidget(PG_WIDGET_SCROLL,PG_DERIVE_BEFORE,wOptionBox);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_BIND,wOptionBox,
+	      0);
+
+  /* Options */
+
+  w24hour = pgNewWidget(PG_WIDGET_CHECKBOX,PG_DERIVE_INSIDE,wOptionBox);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("24-hour clock"),
+	      0);
+  wSeconds = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Show seconds"),
+	      0);
+  wColon = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Flash colon"),
+	      0);
+  wSetFont = pgNewWidget(PG_WIDGET_BUTTON,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Set Font"),
+	      PG_WP_SIDE,PG_S_TOP,
+	      0);
+  wWeekDay = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Show weekday"),
+	      0);
+  wDay = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Show day"),
+	      0);
+  wMonth = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Show month"),
+	      0);
+  wYear = pgNewWidget(PG_WIDGET_CHECKBOX,0,0);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgNewString("Show year"),
+	      /* Give the last one PG_S_ALL to avoid extra margin at the
+	       * bottom of wCheckBoxBox
+	       */
+	      PG_WP_SIDE,PG_S_ALL,
+	      0);
+
+  /* Buttons */
+
+  wCancel = pgNewWidget(PG_WIDGET_BUTTON,PG_DERIVE_INSIDE,wTB);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+				       PGTH_P_STRING_CANCEL),
+	      PG_WP_SIDE,PG_S_RIGHT,
+	      PG_WP_HOTKEY,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_HOTKEY_CANCEL),
+	      PG_WP_BITMAP,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_ICON_CANCEL),
+	      PG_WP_BITMASK,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					  PGTH_P_ICON_CANCEL_MASK),
+	      0);
+  wOk = pgNewWidget(PG_WIDGET_BUTTON,PG_DERIVE_INSIDE,wTB);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+				       PGTH_P_STRING_OK),
+	      PG_WP_SIDE,PG_S_RIGHT,
+	      PG_WP_HOTKEY,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_HOTKEY_OK),
+	      PG_WP_BITMAP,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_ICON_OK),
+	      PG_WP_BITMASK,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					  PGTH_P_ICON_OK_MASK),
+	      0);
+
+  /* Sample clock */
+
+  currentClock.wClock = pgNewWidget(PG_WIDGET_FLATBUTTON,PG_DERIVE_INSIDE,
+				    wSampleTB);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_SIDE,PG_S_RIGHT,
+	      0);
+
+  /* Run the dialog */
+  for (;;) {
+    mungeSettings();                /* New settings take effect */
+    evt = *pgGetEvent();            /* Wait for an event */
+
+    /* Set Font */
+    if (evt.from == wSetFont) {
+      pghandle fNew, fOld;
+      fNew = pgFontPicker("Set Clock Font");
+      if (fNew) {
+	fOld = currentClock.fClockFont;
+	/* Send new font handle to the main context */
+	pgChangeContext(fNew,-1);
+	/* Select new one, delete old one
+	 * (unless this was the original font we entered with.
+	 * Then, keep it around because we might cancel this dialog) */
+	currentClock.fClockFont = fNew;
+	if (fOld != oldData.fClockFont)
+	  pgDelete(fOld);
+      }
+    }
+
+    /* Cancel - revert everything */
+    else if (evt.from == wCancel) {
+      currentClock = oldData;
+      break;
+    }
+
+    /* Done. Revert widget, delete old font */
+    else if (evt.from == wOk) {
+      if (oldData.fClockFont != currentClock.fClockFont)
+	pgDelete(oldData.fClockFont);
+      currentClock.wClock = oldData.wClock;
+      break;
+    }
+  }
+  pgLeaveContext();
+  return 0;
+}
+
+/* Prepare settings for use in updateTime */
+void mungeSettings(void) {
+  unsigned long t;
+
+  /* Set clock font */
+  pgSetWidget(currentClock.wClock,
+	      PG_WP_FONT,currentClock.fClockFont,
+	      0);
+  
+  /* Figure out the necessary update time */
+  if (currentClock.flashColon)
+    t = 500;
+  else if (currentClock.enableSeconds)
+    t = 1000;
+  else
+    t = 60000;
+  pgSetIdle(t,&updateTime);
+
+  /* Build a format string */
+  currentClock.fmt1[0] = 0;              /* Start out blank */
+  if (currentClock.enableWeekDay)
+    strcat(currentClock.fmt1,"%a ");
+  if (currentClock.enableMonth)
+    strcat(currentClock.fmt1,"%b ");
+  if (currentClock.enableDay)
+    strcat(currentClock.fmt1,"%e ");
+  if (currentClock.enable24hour)
+    strcat(currentClock.fmt1,"%k");
+  else
+    strcat(currentClock.fmt1,"%l");
+  strcat(currentClock.fmt1,":%M");
+  if (currentClock.enableSeconds)        /* Careful with the colons... */
+    strcat(currentClock.fmt1,":%S ");
+  else
+    strcat(currentClock.fmt1," ");
+  if (!currentClock.enable24hour)
+    strcat(currentClock.fmt1,"%p ");
+  if (currentClock.enableYear)
+    strcat(currentClock.fmt1,"%Y ");
+  currentClock.fmt1[strlen(currentClock.fmt1)-1] = 0;  /* Chop extra space */
+
+  /* Make secondary format string with optional colon flashing */
+  strcpy(currentClock.fmt2,currentClock.fmt1);
+  if (currentClock.flashColon) {
+    /* Convert all colons to blanks */
+    char *p = currentClock.fmt2;
+    while (p = strchr(p,':'))
+      *p = ' ';
+  }
+
+  /* Resize the applet if necessary */
+  updateTime();
+  pgUpdate();
+}
+
+/* Update the current clock time */
+void updateTime(void) {
    time_t now;
    struct tm *ltime;
-   char buf[20];
+   char buf[CLKMAX];
+   static int blink = 0;
+
    time(&now);
    ltime = localtime(&now);
-   strftime(buf,40,"%l:%M %p",ltime);
-   pgReplaceText(wClock,buf);
-   pgSubUpdate(wClock);
+   strftime(buf,CLKMAX,blink ? currentClock.fmt1 : currentClock.fmt2,ltime);
+   blink = !blink;
+   pgReplaceText(currentClock.wClock,buf);
+   pgSubUpdate(currentClock.wClock);
 }
 
 int main(int argc,char **argv) {
@@ -53,15 +288,22 @@ int main(int argc,char **argv) {
     return 1;
   }
  
-  /* Clock widget */
-  wClock = pgNewWidget(PG_WIDGET_LABEL,PG_DERIVE_INSIDE,wPGLbar);
+  /* Clock widget. Make it a flatbutton so it normally looks fairly
+   * unobtrusive, but it can be clicked. Clicking the clock launches
+   * an options dialog box.
+   */
+  currentClock.wClock = pgNewWidget(PG_WIDGET_FLATBUTTON,
+				    PG_DERIVE_INSIDE,wPGLbar);
   pgSetWidget(PGDEFAULT,
-	      PG_WP_TRANSPARENT,0,
 	      PG_WP_SIDE,PG_S_RIGHT,
 	      0);
+  pgBind(PGDEFAULT,PG_WE_ACTIVATE,&btnDialog,NULL);
 
-  /* Update the clock every 30 seconds */
-  pgSetIdle(30000,&sysIdle);
+  /* Set up default clock */
+  currentClock.fClockFont = pgNewFont(NULL,0,PG_FSTYLE_FIXED);
+  currentClock.enableSeconds = currentClock.enable24hour = 1;
+  mungeSettings();
+
   pgEventLoop();
   return 0;
 }
