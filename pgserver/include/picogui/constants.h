21789d6df4e839473779fc4472cd982064ec9a2a
Micah Elizabeth Scott <micah@misc.name>
2001-10-08 07:08:35 +0000

    Added driver messages and a nonwidget event to facilitate a standard PicoGUI touchscreen calibrator app

--- pgserver/include/picogui/constants.h
+++ pgserver/include/picogui/constants.h
@@ -1,4 +1,4 @@
-/* $Id: constants.h,v 1.89 2001/10/08 04:02:39 micahjd Exp $
+/* $Id: constants.h,v 1.90 2001/10/08 07:08:35 micahjd Exp $
  *
  * picogui/constants.h - various constants needed by client, server,
  *                       and application
@@ -738,6 +738,8 @@ typedef unsigned long pghandle;
 #define PGDM_SDC_CHAR         5   //!< Send a character to the secondary display channel
 #define PGDM_BRIGHTNESS       6   //!< Set display brightness, 0x00-0xFF
 #define PGDM_CONTRAST         7   //!< Set display contrast, 0x00-0xFF
+#define PGDM_INPUT_RAW        8   //!< Send PG_NWE_PNTR_RAW from the specified widget
+#define PGDM_INPUT_SETCAL     9   //!< param is a handle to a new calibration string
 
 #define PG_SND_KEYCLICK       1   //!< Short click
 #define PG_SND_BEEP           2   //!< Terminal beep
@@ -881,6 +883,7 @@ typedef unsigned long pghandle;
 #define PG_NWE_PNTR_UP     0x1205
 #define PG_NWE_PNTR_DOWN   0x1204
 #define PG_NWE_BGCLICK     0x120D /* The user clicked the background widget */
+#define PG_NWE_PNTR_RAW    0x1101 /* Raw coordinates, for tpcal */
 
 /* These are event constants used for networked input drivers. It is a subset
  * of the TRIGGER_* constants in the server, representing only those needed
