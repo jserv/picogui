7a81663e2776fe0b51075ac7093d4a8ca7d6665e
Micah Elizabeth Scott <micah@misc.name>
2001-11-14 08:26:25 +0000

    Updated textdemo to use pgDataString

--- apps/textdemo/textdemo.c
+++ apps/textdemo/textdemo.c
@@ -3,17 +3,23 @@
 #include <picogui.h>
 
 int main(int argc, char **argv) {
-  pghandle wText;
-  struct pgmemdata data = pgFromFile(argv[1]);
-  
+  pghandle wText, str;
+ 
   pgInit(argc,argv);
   pgRegisterApp(PG_APP_NORMAL,"Textbox Test",0);
+
+  /* Load text */
+  str = pgDataString(pgFromFile(argv[1])),
 
   wText = pgNewWidget(PG_WIDGET_TEXTBOX,0,0);
   pgSetWidget(PGDEFAULT,
 	      PG_WP_TEXTFORMAT,pgNewString("HTML"),
-	      PG_WP_TEXT,pgNewString(data.pointer),
+	      PG_WP_TEXT,str,
+	      PG_WP_SIDE,PG_S_LEFT,
 	      0);
+
+  /* The textbox makes its own copy of the text */
+  pgDelete(str);
   
   pgNewWidget(PG_WIDGET_LABEL,0,0);
   pgSetWidget(PGDEFAULT,
