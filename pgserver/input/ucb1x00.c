2adf4245a486ee30f3186a07ad0fd033ba62b4c7
gork <gork>
2001-11-25 01:11:53 +0000

    Cleaned this puppy up, couple little bugfixes, added (braindead) jitter correction support

--- pgserver/input/ucb1x00.c
+++ pgserver/input/ucb1x00.c
@@ -17,8 +17,8 @@
 #include <pgserver/widget.h>    /* for dispatch_pointing */
 #include <pgserver/pgnet.h>
 
-#define SAMPLE_SET_SZ	3	/* how many samples to average */
-#define POLL_USEC	200	/* how long after input is recieved to issue pen up */
+#define SAMPLE_SET_SZ		3	/* how many samples to average */
+#define JITTER_TOLERANCE	3	/* Pixel radius to ignore jitter */
  
 static const char *DEVICE_FILE_NAME = "/dev/ucb1x00-ts";
 static const char *_file_ = __FILE__;
@@ -32,14 +32,18 @@ static int inits_since_data=0;
 /* show the cursor on any touchscreen activity */
 static int showcursor;
 /* Calibration variables read from config file */
-static int res_x, res_y, max_x, max_y, min_x, min_y, flip_x, flip_y;
+static int res_x, res_y, max_x, max_y, min_x, min_y, flip_x, flip_y, tolerance;
 
 /* last place the cursor was and lat time it was there */
 static int last_x;
 static int last_y;
-static struct timeval last_time;
+
+/* these hold the sums of x,y samples to average */
+static int sum_x;
+static int sum_y;
+
 /* counts the number of samples taken (for averaging) */
-static int sample_counter = 0;
+static int sample = 0;
 
 g_error ucb1x00_init(void)
 {
@@ -68,6 +72,7 @@ g_error ucb1x00_init(void)
   min_y = get_param_int("input-ucb1x00","min_y",50);
   flip_x = get_param_int("input-ucb1x00","flip_x",1);
   flip_y = get_param_int("input-ucb1x00","flip_y",1);
+  tolerance = get_param_int("input-ucb1x00","tolerance",45);
   
   return sucess;
 }
@@ -84,31 +89,27 @@ void ucb1x00_close(void)
 }
 
 int ucb1x00_fd_activate(int active_fd) {
-  int bytes_read;
   struct {
-	  u16 p;
+	  u16 pressure;
 	  u16 x;
 	  u16 y;
 	  u16 pad;
 	  struct timeval time;
   } ts_data;
+  int bytes_read;
   int cooked_x, cooked_y;
-  
-  /* these hold the sums of x,y samples to average */
-  static int sum_x;
-  static int sum_y;
        
   if (active_fd != fd) return 0;
 
   /* read a data point */
   bytes_read = read(fd, (char *)&ts_data, sizeof(ts_data));
 
-  /* No data yet */
+  /* No data yet - (shouldnt ever actaully call this)*/
   if (bytes_read = 0) {
-	  printf("NO BYTES READ!!!!!!!!!!!!\n");
 	  return 0;
   }
  
+  /* reset the fd_init call counter so we dont issue pen up */
   inits_since_data=0;
   
   /* cook the coordinates (perform calibration) */
@@ -117,52 +118,56 @@ int ucb1x00_fd_activate(int active_fd) {
   cooked_y = (flip_y) ? ((max_y - ts_data.y) * res_y) / (max_y - min_y) :
                 ((ts_data.y - min_y) * res_y) / (max_y - min_y);
   
-  if(ts_data.p) {
+  if(ts_data.pressure > tolerance) {
 	 
     /* Show the cursor if 'showcursor' is on */
     if (showcursor)
       drivermessage(PGDM_CURSORVISIBLE,1,NULL);
 
-    if(pen_down && sample_counter == SAMPLE_SET_SZ) {
-      last_x=sum_x/SAMPLE_SET_SZ;
-      last_y=sum_y/SAMPLE_SET_SZ;
-      last_time=ts_data.time;
-      dispatch_pointing(TRIGGER_MOVE,last_x,last_y,1);
-      sample_counter = 0;
+    sum_x += cooked_x;
+    sum_y += cooked_y;
+   
+    if(++sample == SAMPLE_SET_SZ) {
+      if( ((cooked_x > last_x) && ((cooked_x - last_x) > JITTER_TOLERANCE)) ||
+          ((cooked_x < last_x) && ((last_x - cooked_x) > JITTER_TOLERANCE)) )
+	      last_x=sum_x/SAMPLE_SET_SZ;
+#ifdef DEBUG_EVENT
+      else printf("Jitter corrected X\n");
+#endif
+      if( ((cooked_y > last_y) && ((cooked_y - last_y) > JITTER_TOLERANCE)) ||
+          ((cooked_y < last_y) && ((last_y - cooked_y) > JITTER_TOLERANCE)) )
+	      last_y=sum_y/SAMPLE_SET_SZ;
+#ifdef DEBUG_EVENT
+      else printf("Jitter corrected Y\n");
+#endif
+
+      sample=0;
       sum_x=0;
       sum_y=0;
+    
+      if(pen_down) {
+        dispatch_pointing(TRIGGER_MOVE,last_x,last_y,1);
 #ifdef DEBUG_EVENT
-      printf("Pen Move\n");
+        printf("Pen Move\n");
 #endif
-    } else if(sample_counter == SAMPLE_SET_SZ) {
-      last_x=sum_x/SAMPLE_SET_SZ;
-      last_y=sum_y/SAMPLE_SET_SZ;
-      last_time=ts_data.time;
-      dispatch_pointing(TRIGGER_DOWN,last_x,last_y,1);
-      pen_down = 1;
-      sample_counter = 0;
-      sum_x=0;
-      sum_y=0;
+      } else {
+        dispatch_pointing(TRIGGER_DOWN,last_x,last_y,1);
+        pen_down=1;
 #ifdef DEBUG_EVENT
-      printf("Pen Down\n");
+        printf("Pen Down\n");
 #endif
-    } else {
-      sum_x += cooked_x;
-      sum_y += cooked_y;
-      sample_counter++;
+      }
     }
-  
   }
 
 #ifdef DEBUG_EVENT
-  printf("(%d,%d:%d:%d) (%d, %d)\n", ts_data.x, ts_data.y, ts_data.p, cooked_x, cooked_y);
+  printf("(%d,%d:%d:%d) (%d, %d)\n", ts_data.x, ts_data.y, ts_data.pressure, cooked_x, cooked_y);
 #endif	
 
   return 1;
 
 }
 
-/* Polling time for the input driver */
 void ucb1x00_fd_init(int *n,fd_set *readfds,struct timeval *timeout) {
   if(fd<0) return;
 
@@ -173,12 +178,17 @@ void ucb1x00_fd_init(int *n,fd_set *readfds,struct timeval *timeout) {
     FD_SET(fd, readfds);
 
   /* a little unorthodox to do this here, but... simpler? */
-  if(inits_since_data++ > 2 && pen_down) {
+  if(inits_since_data++ == 3) {
+	  if (pen_down) {
 #ifdef DEBUG_EVENT
-	  printf("Pen Up\n");
+		  printf("Pen Up\n");
 #endif
-	  dispatch_pointing(TRIGGER_UP,last_x,last_y,0);
-	  pen_down = 0;
+		  dispatch_pointing(TRIGGER_UP,last_x,last_y,0);
+		  pen_down = 0;
+	  }
+	  sample=0;
+	  sum_x=0;
+	  sum_y=0;
   }
   
 }
