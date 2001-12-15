7394d001c96cda6d67efce11a42939dd66cb9f13
carpman <carpman>
2001-12-15 23:52:30 +0000

    First useable revision.

--- apps/pgl/pgl-launcher/pgl-launcher.c
+++ apps/pgl/pgl-launcher/pgl-launcher.c
@@ -27,9 +27,14 @@
 #include <picogui.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #include "pgl-launcher.h"
 #include "configfile.h"
+
+char *appDir;
+char *confPath;
 
 int directoryScan(char *path){
   int appCount = 0, appCopy = 0;
@@ -45,14 +50,12 @@ int directoryScan(char *path){
     while((dent = readdir(d))){
       sprintf(absPath, "%s/%s/app.conf", path, dent->d_name);
       if(configfile_parse(absPath)){
-	printf("Got one config.\n");
 	appCount++;
 	configfile_free();
       }
     }
     
     if(appCount > 0){
-      printf("Parsing configs\n");
       gAppList = (struct pgllApp *)malloc((sizeof(struct pgllApp)*appCount));
       rewinddir(d);
       while((dent = readdir(d))){
@@ -87,38 +90,140 @@ int directoryScan(char *path){
   }
 }
 
+void freeApplication(struct pgllApp *app){
+  free(app->appName);
+  free(app->appPath);
+  free(app->appIcon);
+}
+
+void parseConfig(char *configfile){
+  char *appDirTmp;
+
+  configfile_parse("launcher.conf");
+  if((appDirTmp = get_param_str("Applications", "appdirPath", NULL))){
+    appDir = malloc(strlen(appDirTmp)+1);
+    strcpy(appDir, appDirTmp);
+  }else{
+    appDir = malloc(strlen("/usr/local/apps")+1);
+    strcpy(appDir, "/usr/local/apps");
+  }
+  configfile_free();
+}
+
+void childDied(int foo){ 
+
+  waitpid(-1, NULL, WNOHANG); 
+}
+
 int runApp(char *appPath){
   
-  printf("Would execute %s\n", appPath);
+  signal(SIGCHLD, childDied);
 
-  if(!vfork())
+  if(!vfork()){
     execl(appPath, NULL);
+  }
   
   return 1;
 }
 
-int addItem(struct pgEvent *evt){return 0;}
+int setPreferences(void){
+  pghandle toolbar, optionBox;
+  pghandle appPath;
+  pghandle cancel, ok;
+  struct pgEvent evt;
 
-int removeItem(struct pgEvent *evt){return 0;}
+  pgEnterContext();
+
+  pgDialogBox("Launcher Preferences");
+  
+  toolbar = pgNewWidget(PG_WIDGET_TOOLBAR, 0, 0);
+  pgSetWidget(toolbar, PG_WP_SIDE, PG_S_BOTTOM, 0);
+
+  optionBox = pgNewWidget(PG_WIDGET_BOX, 0, 0);
+  pgNewWidget(PG_WIDGET_SCROLL, PG_DERIVE_BEFORE, optionBox);
+  pgSetWidget(PGDEFAULT, PG_WP_BIND, optionBox, 0);
+
+  appPath = pgNewWidget(PG_WIDGET_FIELD, PG_DERIVE_INSIDE, optionBox);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT, pgNewString(appDir),
+	      0);
+  pgNewWidget(PG_WIDGET_LABEL, PG_DERIVE_INSIDE, optionBox);
+  pgSetWidget(PGDEFAULT, 
+	      PG_WP_TEXT, pgNewString("App path:"), 
+	      0);
+
+  cancel = pgNewWidget(PG_WIDGET_BUTTON,PG_DERIVE_INSIDE,toolbar);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+				       PGTH_P_STRING_CANCEL),
+	      PG_WP_SIDE,PG_S_RIGHT,
+	      PG_WP_HOTKEY,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_HOTKEY_CANCEL),
+	      PG_WP_BITMAP,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_ICON_CANCEL),
+	      PG_WP_BITMASK,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					  PGTH_P_ICON_CANCEL_MASK),
+	      0);
+  ok = pgNewWidget(PG_WIDGET_BUTTON,PG_DERIVE_INSIDE,toolbar);
+  pgSetWidget(PGDEFAULT,
+	      PG_WP_TEXT,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+				       PGTH_P_STRING_OK),
+	      PG_WP_SIDE,PG_S_RIGHT,
+	      PG_WP_HOTKEY,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_HOTKEY_OK),
+	      PG_WP_BITMAP,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					 PGTH_P_ICON_OK),
+	      PG_WP_BITMASK,pgThemeLookup(PGTH_O_POPUP_MESSAGEDLG,
+					  PGTH_P_ICON_OK_MASK),
+	      0);
+
+  for(;;){
+    evt = *pgGetEvent();            /* Wait for an event */
+
+    if(evt.from == cancel){
+      break;
+    }else if(evt.from == ok){
+      configfile_parse("launcher.conf");
+      set_param_str("Applications", "appdirPath", pgGetString(pgGetWidget(appPath,PG_WP_TEXT)));
+      configfile_write("launcher.conf");
+      configfile_free();
+      //We need to refresh our config data now
+      parseConfig("launcher.conf");
+      break;
+    }
+  }
+  pgLeaveContext();
+
+  return 0;
+}
 
 int launchMenu(struct pgEvent *evt){
   pghandle *items;
   int item;
 
-  printf("Generating menu\n");
-
   pgEnterContext();
 
-  items = alloca(sizeof(pghandle) * gAppCount);
+  items = alloca(sizeof(pghandle) * gAppCount+2);
 
   for(item = 0; item < gAppCount; item++){
     items[item] = pgNewString(gAppList[item].appName);
   }
 
-  item = pgMenuFromArray(items, gAppCount);
+  //Add the standard options
+  items[gAppCount] = pgNewString("Rescan Application Directory");
+  items[gAppCount+1] = pgNewString("Preferences...");
 
-  if(item)
-    runApp(gAppList[item-1].appPath);
+  item = pgMenuFromArray(items, gAppCount+2);
+
+  if(item){
+    if(item == gAppCount+2){
+      setPreferences();
+    }else if(item == gAppCount+1){
+      directoryScan(appDir);
+    }else{
+      runApp(gAppList[item-1].appPath);
+    }
+  }
 
   pgLeaveContext();
  
@@ -127,11 +232,17 @@ int launchMenu(struct pgEvent *evt){
 
 int main(int argc, char **argv){
   pghandle pglBar;
+  int freeLoop;
 
   pgInit(argc, argv);
 
-  directoryScan("/usr/local/apps");
+  //Open our config file and parse it
+  parseConfig("launcher.conf");
 
+  //Find applications
+  directoryScan(appDir);
+
+  //Install into the toolbar
   pglBar = pgFindWidget("PGL-AppletBar");
   if(!pglBar){
     pgMessageDialog(argv[0], "This applet requires PGL",PG_MSGICON_ERROR);
@@ -147,6 +258,10 @@ int main(int argc, char **argv){
   pgBind(PGDEFAULT, PG_WE_PNTR_DOWN, &launchMenu, NULL);
 
   pgEventLoop();
+
+  for(freeLoop = 0; freeLoop < gAppCount; freeLoop++){
+    freeApplication(&gAppList[freeLoop]);
+  }
  
   free(gAppList);
   
