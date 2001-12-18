a85769edc9cbc5d33728ba1ffd590eb5d98e3f87
lonetech <lonetech>
2001-12-18 14:56:37 +0000

    Added max_lines preference for terminal. Scrolling doesn't work right yet, so keep your max_lines xchat.conf setting as 0.

--- ports/xchat/src/fe-picogui/fe-picogui.c
+++ ports/xchat/src/fe-picogui/fe-picogui.c
@@ -151,6 +151,8 @@ fe_new_window (struct session *sess)
 			pgBind(0, PG_WE_DATA, evtPassFocus,
 					(void*)sess->gui->input);
 			pgSetWidget(0, PG_WP_SIDE, PG_S_ALL, 0);
+			if(prefs.max_lines)
+				pgSetWidget(0, PG_WP_LINES, prefs.max_lines, 0);
 			/* hide cursor */
 			pgWriteData(0, pgFromMemory("\e[?25l", 6));
 			break;
