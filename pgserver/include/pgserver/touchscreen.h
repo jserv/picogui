80b6d7575720d89d21b41ef72996e76aac13b275
lonetech <lonetech>
2002-01-08 22:53:07 +0000

    Clarified comments a bit.

--- pgserver/include/pgserver/touchscreen.h
+++ pgserver/include/pgserver/touchscreen.h
@@ -3,16 +3,15 @@
 #ifdef CONFIG_TOUCHSCREEN
 /* Touchscreen helpers */
 
-/* converts pen coordinates to physical; safe to call, since it checks 
- * touchscreen_calibrated */
+/* converts pen coordinates to physical; always call this, as it sends the
+ * raw coordinates to tpcal when touchscreen_calibrated is unset */
 void touchscreen_pentoscreen(int *x, int *y);
 /* message function for the calibration codes */
 void touchscreen_message(u32 message, u32 param, u32 *ret);
 /* loads touchscreen calibration - CALL THIS! */
 g_error touchscreen_init(void);
 /* 1 if touchscreen has been calibrated
- * if 0, pgserver could run tpcal automatically
- * don not process coordinates in any way while this is 0 */
+ * if 0, pgserver could run tpcal automatically */
 extern u8 touchscreen_calibrated;
 #endif
 #endif
