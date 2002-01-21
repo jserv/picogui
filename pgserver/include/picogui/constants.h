e2d0c24f4a1dded5559f8cf9dd108665540bf921
cgrigis <cgrigis>
2002-01-21 09:46:28 +0000

    added PG_TRIGGER_[DE]ACTIVATE constants to the right place this time :)

--- pgserver/include/picogui/constants.h
+++ pgserver/include/picogui/constants.h
@@ -1,4 +1,4 @@
-/* $Id: constants.h,v 1.119 2002/01/20 08:09:29 micahjd Exp $
+/* $Id: constants.h,v 1.120 2002/01/21 09:45:51 cgrigis Exp $
  *
  * picogui/constants.h - various constants needed by client, server,
  *                       and application
@@ -977,6 +977,8 @@ typedef unsigned long pghandle;
 /* These are event constants used for networked input drivers. It is a subset
  * of the TRIGGER_* constants in the server, representing only those needed
  * for input drivers. */
+#define PG_TRIGGER_ACTIVATE   (1<<3)  /* Sent when it receives focus */
+#define PG_TRIGGER_DEACTIVATE (1<<4)  /* Losing focus */
 #define PG_TRIGGER_KEYUP      (1<<5)  /* Ignores autorepeat, etc. Raw key codes*/
 #define PG_TRIGGER_KEYDOWN    (1<<6)  /* Ditto. */
 #define PG_TRIGGER_UP         (1<<8)  /* Mouse up */
