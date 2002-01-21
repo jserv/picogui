fd92c5ed98708ff59fb3960abcbda33171236343
Micah Elizabeth Scott <micah@misc.name>
2002-01-21 14:39:09 +0000

    Oopsie, forgot to add these to CVS a while ago

---
+++ pgserver/input/mgl2input.c
@@ -0,0 +1,180 @@
+/* $Id: mgl2input.c,v 1.1 2002/01/21 14:39:09 micahjd Exp $
+ *
+ * mgl2input.c - input driver for MGL2
+ *
+ * PicoGUI small and efficient client/server GUI
+ * Copyright (C) 2000,2001 Micah Dowty <micahjd@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Contributors:
+ *               Hiroyuki Yanai <fk200329@fsinet.or.jp>
+ * 
+ * 
+ * 
+ */
+
+/*
+ * MGL -- MobileGear Graphic Library -
+ * Copyright (C) 1998, 1999, 2000, 2001
+ *      Koji Suzuki (suz@at.sakura.ne.jp)
+ *      Yukihiko Sano (yukihiko@yk.rim.or.jp)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY KOJI SUZUKI AND YUKIHIKO SANO AND
+ * CONTRIBUTORS``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <pgserver/common.h>
+
+#ifdef DRIVER_MGL2INPUT
+
+#include <pgserver/input.h>
+#include <pgserver/widget.h>
+#include <pgserver/pgnet.h>
+
+#include <mgl2.h>
+#include <mglkey.h>
+
+static int mx, my, mv, mbtn;
+
+#define vkloc2pointing(state, btn) { \
+ if (vk_x > 0 && vk_x < SCREEN_WIDTH) { \
+   if (vk_y > 0 && vk_y < SCREEN_HEIGHT) { \
+     mx = vk_x; \
+     my = vk_y; \
+   } \
+ } \
+ dispatch_pointing((state), mx, my, (btn)); \
+}
+
+void mgl2input_poll(void) {
+  int key, modkey;
+
+  key = get_key_im(0);
+  if (key == -1)
+    return;
+
+  switch (key) {
+  case MK_V1:
+    vkloc2pointing(TRIGGER_DOWN, 1);
+    break;
+  case MK_V2:
+    vkloc2pointing(TRIGGER_MOVE, 1);
+    break;
+  case MK_V3:
+    vkloc2pointing(TRIGGER_UP, 0);
+    break;
+  case MK_F1:
+  case MK_LEFT:
+    mx -= mv;
+    if (mx < 0)
+      mx = 0;
+    dispatch_pointing(TRIGGER_MOVE, mx, my, 1);
+    break;
+  case MK_F2:
+  case MK_DOWN:
+    my += mv;
+    if (my > SCREEN_HEIGHT)
+      my = SCREEN_HEIGHT;
+    dispatch_pointing(TRIGGER_MOVE, mx, my, 1);
+    break;
+  case MK_F3:
+  case MK_UP:
+    my -= mv;
+    if (my < 0)
+      my = 0;
+    dispatch_pointing(TRIGGER_MOVE, mx, my, 1);
+    break;
+  case MK_F4:
+  case MK_RIGHT:
+    mx += mv;
+    if (mx > SCREEN_WIDTH)
+      mx = SCREEN_WIDTH;
+    dispatch_pointing(TRIGGER_MOVE, mx, my, 1);
+    break;
+  case MK_F5:
+    /*  case 0x20: */
+    if (mbtn == 0) {
+      dispatch_pointing(TRIGGER_DOWN, mx, my, 1);
+      get_key(-1);
+      mbtn = 1;
+    } else {
+      dispatch_pointing(TRIGGER_UP, mx, my, 0);
+      get_key(-1);
+      mbtn = 0;
+    }
+    break;
+  case MK_F6:
+  case MK_NUMLOCK:
+    mv = (mv > 30) ? (1) : (mv + 5);
+    break;
+  default:
+    modkey = 0;
+
+#if 0
+    if (key & MGL_SKM_CAPS)
+      modkey = PGMOD_CAPS;
+    else if (key & MGL_SKM_SHIFT) 
+      modkey = PGMOD_SHIFT;
+    else if (key & MGL_SKM_CTRL)
+      modkey = PGMOD_CTRL;
+    else if (key & MGL_SKM_ALT)
+      modkey = PGMOD_ALT;
+
+    dispatch_key(TRIGGER_CHAR, key & ~MGL_SKM_MASK, modkey);
+#else
+    dispatch_key(TRIGGER_CHAR, key, modkey);
+#endif
+    break;
+  }
+}
+
+g_error mgl2input_init(void) {
+  mx = SCREEN_WIDTH / 2;
+  my = SCREEN_HEIGHT / 2;
+  mv = 1;
+  mbtn = 0;
+  return success;
+}
+
+g_error mgl2input_regfunc(struct inlib *i) {
+  i->init = &mgl2input_init;
+  i->poll = &mgl2input_poll;
+  return success;
+}
+
+#endif /* DRIVER_MGL2INPUT */
+
+/* The End */
+
---
+++ pgserver/video/mgl2fb.c
@@ -0,0 +1,188 @@
+/* $Id: mgl2fb.c,v 1.1 2002/01/21 14:39:09 micahjd Exp $
+ *
+ * mgl2fb.c - Video driver for MGL2
+ *
+ * PicoGUI small and efficient client/server GUI
+ * Copyright (C) 2000,2001 Micah Dowty <micahjd@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Contributors:
+ *               Hiroyuki Yanai <fk200329@fsinet.or.jp>
+ * 
+ * 
+ * 
+ */
+
+/*
+ * MGL -- MobileGear Graphic Library -
+ * Copyright (C) 1998, 1999, 2000, 2001
+ *      Koji Suzuki (suz@at.sakura.ne.jp)
+ *      Yukihiko Sano (yukihiko@yk.rim.or.jp)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY KOJI SUZUKI AND YUKIHIKO SANO AND
+ * CONTRIBUTORS``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <pgserver/common.h>
+
+#ifdef DRIVER_MGL2FB
+
+#include <pgserver/video.h>
+#include <pgserver/input.h>
+
+#include <mgl2.h>
+#include <mglcol.h>
+
+/* Macros to easily access the members of vid->display */
+#define FB_MEM   (((struct stdbitmap*)vid->display)->bits)
+#define FB_BPL   (((struct stdbitmap*)vid->display)->pitch)
+
+static struct virtual_key *vk;
+static int mc_plt[258];
+
+g_error mgl2fb_init(void);
+void mgl2fb_close(void);
+void mgl2fb_update(s16 x, s16 y, s16 w, s16 h);
+g_error mgl2fb_regfunc(struct vidlib *v);
+
+
+g_error mgl2fb_init(void) {
+  g_error e;
+  int r, g, b, i;
+
+  FB_MEM = NULL;
+
+  SCREEN_WIDTH = 640;
+  SCREEN_HEIGHT = 480;
+
+  if (!open_graph())
+    return mkerror(PG_ERRT_IO, 46);
+
+  vk = create_virtual_key3(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, MK_V1, MK_V2, MK_V3);
+  if (!vk)
+    return mkerror(PG_ERRT_IO, 46);
+
+  vk_attach(NULL, vk);
+
+  e = g_malloc((void **) &FB_MEM, (SCREEN_WIDTH + 1) * (SCREEN_HEIGHT + 1));
+  errorcheck;
+
+  vid->xres = SCREEN_WIDTH;
+  vid->yres = SCREEN_HEIGHT;
+  vid->bpp = 8;
+  FB_BPL = SCREEN_WIDTH;
+  setvbl_linear8(vid);
+
+  for (i = 0; i < 256; i++)
+    mc_plt[i] = mc_from_rgb(packRGB(((i >> 4) & 0x0c), ((i >> 2) & 0x0e), ((i << 1) & 0x0e)));
+
+#if 0
+  mgl_set_keymode(MGL_SK_EXTRANSLATED);
+#endif
+
+  /* Load a main input driver */
+  return load_inlib(&mgl2input_regfunc,&inlib_main);
+}
+
+void mgl2fb_close(void) {
+  /* Free backbuffer */
+  if (FB_MEM)
+    g_free(FB_MEM);
+
+  if (vk)
+    vk_detach(vk, 1);
+
+  unload_inlib(inlib_main);   /* Take out our input driver */
+  close_graph();
+}
+
+void mgl2fb_update(s16 x, s16 y, s16 w, s16 h) {
+  u8 *srcptr, *ptr;
+  int xx, xleft, i, j;
+  int pixbuf[8];
+
+  if (!FB_MEM)
+    return;
+
+#if 0
+  srcptr = FB_MEM + y * FB_BPL + x;
+  for (i = 0; i < h; i++) {
+    xx = x;
+    ptr = srcptr;
+    for (j = 0; j < w; j++) {
+      put_pixel(xx++, y, mc_plt[*ptr++]);
+    }
+    y++;
+    srcptr += FB_BPL;
+  }
+#endif
+
+  xleft = w % 8;
+  srcptr = FB_MEM + y * FB_BPL + x;
+  for (i = 0; i < h; i++) {
+    xx = x;
+    ptr = srcptr;
+    for (j = 0; j < w; j += 8, xx += 8) {
+      pixbuf[0] = mc_plt[*ptr++];
+      pixbuf[1] = mc_plt[*ptr++];
+      pixbuf[2] = mc_plt[*ptr++];
+      pixbuf[3] = mc_plt[*ptr++];
+      pixbuf[4] = mc_plt[*ptr++];
+      pixbuf[5] = mc_plt[*ptr++];
+      pixbuf[6] = mc_plt[*ptr++];
+      pixbuf[7] = mc_plt[*ptr++];
+
+      put_pixstream(xx, y, pixbuf, 8, DIR_NORTH);
+    }
+
+    for (j = 0; j < xleft; j++)
+      put_pixel(xx++, y, mc_plt[*ptr++]);
+
+    y++;
+    srcptr += FB_BPL;
+  }
+}
+
+g_error mgl2fb_regfunc(struct vidlib *v) {
+  v->init = &mgl2fb_init;
+  v->close = &mgl2fb_close;
+  v->update = &mgl2fb_update;
+  return success;
+}
+
+#endif /* DRIVER_MGL2FB */
+
+/* The End */
+
