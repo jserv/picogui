5edeb749e9a042ef8e3337a6bb94b9d64156a361
lonetech <lonetech>
2002-01-24 03:28:03 +0000

    Use the new "property name=id;" syntax. Custom properties in .h files are now readable from fillstyle code.

--- apps/gridgame/gridth.h
+++ apps/gridgame/gridth.h
@@ -1,5 +1,16 @@
+#ifdef THEMESOURCE
+#define PROPDECL(name,id) prop name=id;
+#else
+#define PROPDECL(name,id) /*empty*/
+#endif
+
 #define SCALE 100
+
 #define BGEVEN PGTH_P_USER
 #define BGODD PGTH_P_USER+1
 #define PIECE PGTH_P_USER+2
 #define GRIDGAME_USER PGTH_P_USER+1000
+
+PROPDECL(bgeven,BGEVEN)
+PROPDECL(bgodd,BGODD)
+PROPDECL(piece,PIECE)
