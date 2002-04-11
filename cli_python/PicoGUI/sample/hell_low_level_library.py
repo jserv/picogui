8c61a138e5591750258e9c51bf0d09a7ee27c4a7
lalo <lalo>
2002-04-11 07:58:56 +0000

    Now that I showd the low-level Hello, World to my friend, I was ashamed of all the pg_in stuff and removed it.

--- cli_python/PicoGUI/sample/hell_low_level_library.py
+++ cli_python/PicoGUI/sample/hell_low_level_library.py
@@ -5,40 +5,38 @@ from PicoGUI import network, requests, responses, events
 
 def test(where='localhost', port=0):
   connection = network.sock(where, port)
-  pg_in = connection.makefile()
   connection.send(requests.mkstring('Greetings'))
-  string_id = responses.next(pg_in)
+  string_id = responses.next(connection)
   connection.send(requests.register(string_id))
-  top_id = responses.next(pg_in)
+  top_id = responses.next(connection)
   # PG_WIDGET_LABEL is 1
   connection.send(requests.createwidget(1))
-  label_id = responses.next(pg_in)
+  label_id = responses.next(connection)
   connection.send(requests.mkstring('Hello, World'))
-  string_id = responses.next(pg_in)
+  string_id = responses.next(connection)
   # PG_WP_TEXT is 7
   connection.send(requests.set(label_id, 7, string_id))
-  responses.next(pg_in)
+  responses.next(connection)
   # PG_WP_SIDE is 2, PG_S_ALL is 2048 (1<<11)
   connection.send(requests.set(label_id, 2, 2048))
-  responses.next(pg_in)
+  responses.next(connection)
   # PG_FSTYLE_BOLD is 256 (1<<8)
   connection.send(requests.mkfont('', 256, 24))
-  font_id = responses.next(pg_in)
+  font_id = responses.next(connection)
   # PG_WP_FONT is 8
   connection.send(requests.set(label_id, 8, font_id))
-  responses.next(pg_in)
+  responses.next(connection)
   # PG_DERIVE_INSIDE is 2
   connection.send(requests.attachwidget(top_id, label_id, 2))
-  responses.next(pg_in)
+  responses.next(connection)
   connection.send(requests.update())
-  responses.next(pg_in)
+  responses.next(connection)
   return connection
 
 def event_loop(connection):
-  pg_in = connection.makefile()
   while 1:
     connection.send(requests.wait())
-    ev = responses.next(pg_in)
+    ev = responses.next(connection)
     if isinstance(ev, events.Event) and ev.name == 'close':
       return
 
