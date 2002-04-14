c683cb6d096f874b83ba9a7801434cdc0e11154c
Micah Elizabeth Scott <micah@misc.name>
2002-04-14 19:19:46 +0000

    A possibly working touchscreen driver...

--- pgserver/input/ucb1x00.c
+++ pgserver/input/ucb1x00.c
@@ -3,8 +3,8 @@
  * Written by: John Laur
  *
  * This supports "new style" ucb1x00 device driver that does not include
- * jitter correction or coordinate calibration. You have to set these
- * values up in pgserver.conf before pgserver is started!
+ * jitter correction or coordinate calibration. This driver now uses
+ * pgserver's standard mechanisms for calibration and filtering.
  *
  * Requires ucb1x00-ts kernel driver
  */
@@ -26,23 +26,14 @@ static const char *DEVICE_FILE_NAME = "/dev/ucb1x00-ts";
 /* file descriptor for touch panel */
 static int fd = -1;
 static int pen_down=0;
-static int inits_since_data=0;
+static int polls_since_data=0;
 
 /* show the cursor on any touchscreen activity */
-static int showcursor;
-/* Calibration variables read from config file */
-static int res_x, res_y, max_x, max_y, min_x, min_y, flip_x, flip_y, tolerance;
+static int showcursor, pressure_tolerance;
 
 /* last place the cursor was and lat time it was there */
 static int last_x;
 static int last_y;
-
-/* these hold the sums of x,y samples to average */
-static int sum_x;
-static int sum_y;
-
-/* counts the number of samples taken (for averaging) */
-static int sample = 0;
 
 g_error ucb1x00_init(void)
 {
@@ -57,21 +48,9 @@ g_error ucb1x00_init(void)
     return mkerror(PG_ERRT_IO, 74);
   }
 	
-#ifdef DEBUG_EVENT
-  printf("Opened the touchscreen on %d\n", fd);
-#endif
-
   /* Store config for later */
   showcursor = get_param_int("input-ucb1x00","showcursor",0);
-  res_x = get_param_int("input-ucb1x00","res_x",640);
-  res_y = get_param_int("input-ucb1x00","res_y",480);
-  max_x = get_param_int("input-ucb1x00","max_x",950);
-  max_y = get_param_int("input-ucb1x00","max_y",950);
-  min_x = get_param_int("input-ucb1x00","min_x",50);
-  min_y = get_param_int("input-ucb1x00","min_y",50);
-  flip_x = get_param_int("input-ucb1x00","flip_x",1);
-  flip_y = get_param_int("input-ucb1x00","flip_y",1);
-  tolerance = get_param_int("input-ucb1x00","tolerance",45);
+  pressure_tolerance = get_param_int("input-ucb1x00","pressure",45);
   
   return success;
 }
@@ -79,126 +58,131 @@ g_error ucb1x00_init(void)
 void ucb1x00_close(void)
 {
   /* Close the touch panel device. */
-#ifdef DEBUG_EVENT
-  printf("ucb1x00_close called\n");
-#endif
   if (fd >= 0)
     close(fd);
   fd = -1;
 }
 
+/* Internal function to handle filtering and dispatching a packet */
+int ucb1x00_packet(int x, int y, int pressure) {
+  int old_pendown;
+
+  last_x = x;
+  last_y = y;
+
+  /* Transform and filter */
+  old_pendown = pen_down;
+  pen_down = pressure > pressure_tolerance;
+  if (touchscreen_filter(&x, &y, pen_down))
+    return 1;
+  touchscreen_pentoscreen(&x, &y);
+  
+  /* Show the cursor if 'showcursor' is on */
+  if (showcursor)
+    drivermessage(PGDM_CURSORVISIBLE,1,NULL);
+  
+  if(pen_down && old_pendown) {
+    dispatch_pointing(TRIGGER_MOVE,x,y,1);
+  }
+  else if (pen_down && !old_pendown) {
+    dispatch_pointing(TRIGGER_DOWN,x,y,1);
+  }
+  else if (!pen_down && old_pendown) {
+    dispatch_pointing(TRIGGER_UP,x,y,1);
+  }
+}
+
+
 int ucb1x00_fd_activate(int active_fd) {
   struct {
-	  u16 pressure;
-	  u16 x;
-	  u16 y;
-	  u16 pad;
-	  struct timeval time;
+    u16 pressure;
+    u16 x;
+    u16 y;
+    u16 pad;
+    struct timeval time;
   } ts_data;
   int bytes_read;
-  int cooked_x, cooked_y;
-       
+   
   if (active_fd != fd) return 0;
 
   /* read a data point */
   bytes_read = read(fd, (char *)&ts_data, sizeof(ts_data));
 
   /* No data yet - (shouldnt ever actaully call this)*/
-  if (bytes_read == 0) {
-	  return 0;
-  }
+  if (bytes_read == 0)
+    return 0;
  
   /* reset the fd_init call counter so we dont issue pen up */
-  inits_since_data=0;
-  
-  /* cook the coordinates (perform calibration) */
-  cooked_x = (flip_x) ? ((max_x - ts_data.x) * res_x) / (max_x - min_x) :
-                ((ts_data.x - min_x) * res_x) / (max_x - min_x);
-  cooked_y = (flip_y) ? ((max_y - ts_data.y) * res_y) / (max_y - min_y) :
-                ((ts_data.y - min_y) * res_y) / (max_y - min_y);
-  
-  if(ts_data.pressure > tolerance) {
-	 
-    /* Show the cursor if 'showcursor' is on */
-    if (showcursor)
-      drivermessage(PGDM_CURSORVISIBLE,1,NULL);
+  polls_since_data=0;
 
-    sum_x += cooked_x;
-    sum_y += cooked_y;
-   
-    if(++sample == SAMPLE_SET_SZ) {
-      if( ((cooked_x > last_x) && ((cooked_x - last_x) > JITTER_TOLERANCE)) ||
-          ((cooked_x < last_x) && ((last_x - cooked_x) > JITTER_TOLERANCE)) )
-	      last_x=sum_x/SAMPLE_SET_SZ;
-#ifdef DEBUG_EVENT
-      else printf("Jitter corrected X\n");
-#endif
-      if( ((cooked_y > last_y) && ((cooked_y - last_y) > JITTER_TOLERANCE)) ||
-          ((cooked_y < last_y) && ((last_y - cooked_y) > JITTER_TOLERANCE)) )
-	      last_y=sum_y/SAMPLE_SET_SZ;
-#ifdef DEBUG_EVENT
-      else printf("Jitter corrected Y\n");
-#endif
-
-      sample=0;
-      sum_x=0;
-      sum_y=0;
-    
-      if(pen_down) {
-        dispatch_pointing(TRIGGER_MOVE,last_x,last_y,1);
-#ifdef DEBUG_EVENT
-        printf("Pen Move\n");
-#endif
-      } else {
-        dispatch_pointing(TRIGGER_DOWN,last_x,last_y,1);
-        pen_down=1;
-#ifdef DEBUG_EVENT
-        printf("Pen Down\n");
-#endif
-      }
-    }
-  }
-
-#ifdef DEBUG_EVENT
-  printf("(%d,%d:%d) (%d, %d)\n", ts_data.x, ts_data.y, ts_data.pressure, cooked_x, cooked_y);
-#endif	
+  ucb1x00_packet(ts_data.x, ts_data.y, ts_data.pressure);
 
   return 1;
-
 }
 
 void ucb1x00_fd_init(int *n,fd_set *readfds,struct timeval *timeout) {
   if(fd<0) return;
-
+  
   if ((*n)<(fd+1))
     *n = fd+1;
-
+ 
   if (fd>0)
     FD_SET(fd, readfds);
 
-  /* a little unorthodox to do this here, but... simpler? */
-  if(inits_since_data++ == 3) {
-	  if (pen_down) {
-#ifdef DEBUG_EVENT
-		  printf("Pen Up\n");
-#endif
-		  dispatch_pointing(TRIGGER_UP,last_x,last_y,0);
-		  pen_down = 0;
-	  }
-	  sample=0;
-	  sum_x=0;
-	  sum_y=0;
+  /* Set us up a polling time for detecting penups */
+  if (pen_down) {
+    timeout->tv_sec = 0;
+    if (timeout->tv_usec > 300)
+      timeout->tv_usec = 300;
   }
-  
+}
+
+void ucb1x00_poll(void) {
+  /* If it's been a while since we've gotten a sample, release the pen */
+  if (polls_since_data++ >= 3 && pen_down) {
+    ucb1x00_packet(last_x,last_y,0);
+    pen_down = 0;
+  }
+}
+
+void ucb1x00_message(u32 message, u32 param, u32 *ret) {
+  switch (message) {
+    
+    /* Tuxscreen ioctls:
+     *
+     * ioctl 64 -> backlight power
+     * ioctl 65 -> 0x00-0xFF set brightness
+     * ioctl 66 -> get brightness
+     * ioctl 67 -> 0x00-0xFF set contrast
+     * ioctl 68 -> get contrast
+     */
+    
+    /* Control backlight through the power control driver message */
+  case PGDM_POWER:
+    ioctl(fd,64,param > PG_POWER_VIDBLANK);
+    break;
+  case PGDM_BRIGHTNESS:
+    ioctl(fd,65,param);
+    break;
+  case PGDM_CONTRAST:
+    ioctl(fd,67,param);
+    break;
+    
+  default:
+    touchscreen_message(message,param,ret);
+  }
 }
 
 /******************************************** Driver registration */
- 
+  
 g_error ucb1x00_regfunc(struct inlib *i) {
   i->init = &ucb1x00_init;
   i->close = &ucb1x00_close;
   i->fd_activate = &ucb1x00_fd_activate;
   i->fd_init = &ucb1x00_fd_init;
+  i->poll = &ucb1x00_poll;
+  i->message = &ucb1x00_message;
+
   return success;
 }
 
