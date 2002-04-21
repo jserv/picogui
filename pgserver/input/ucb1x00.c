99855e62dc0348c563df40f4b423242b420f3135
Micah Elizabeth Scott <micah@misc.name>
2002-04-21 04:36:12 +0000

    This seems to have fixed the tuxscreen's touchscreen

--- pgserver/input/ucb1x00.c
+++ pgserver/input/ucb1x00.c
@@ -1,6 +1,7 @@
 /*
  * PicoGUI driver for Philips IS2630 AKA Shannon UCB1200 touchscreen
  * Written by: John Laur
+ * (And then really hacked up by Micah...)
  *
  * This supports "new style" ucb1x00 device driver that does not include
  * jitter correction or coordinate calibration. This driver now uses
@@ -23,30 +24,26 @@
 #endif
 #include <pgserver/debug.h>
 
-#define SAMPLE_SET_SZ		3	/* how many samples to average */
-#define JITTER_TOLERANCE	3	/* Pixel radius to ignore jitter */
- 
 static const char *DEVICE_FILE_NAME = "/dev/ucb1x00-ts";
 
 /* file descriptor for touch panel */
 static int fd = -1;
-static int pen_down=0;
-static int polls_since_data=0;
 
 /* show the cursor on any touchscreen activity */
-static int showcursor, pressure_tolerance;
-
-/* last place the cursor was and lat time it was there */
-static int last_x;
-static int last_y;
+static int showcursor;
+static int pressure_tolerance;
 
 g_error ucb1x00_init(void)
 {
+  g_error e;
+  
+  e=touchscreen_init();
+  errorcheck;
+
   /*
    * open up the touch-panel device.
    * Return the fd if successful, or negative if unsuccessful.
    */
-
   fd = open(DEVICE_FILE_NAME, O_NONBLOCK);
   if (fd < 0) {
     DBG("Error %d opening touch panel\n", errno);
@@ -55,7 +52,7 @@ g_error ucb1x00_init(void)
 	
   /* Store config for later */
   showcursor = get_param_int("input-ucb1x00","showcursor",0);
-  pressure_tolerance = get_param_int("input-ucb1x00","pressure",45);
+  pressure_tolerance = get_param_int("input-ucb1x00","pressure",100);
   
   return success;
 }
@@ -69,34 +66,38 @@ void ucb1x00_close(void)
 }
 
 /* Internal function to handle filtering and dispatching a packet */
-int ucb1x00_packet(int x, int y, int pressure) {
-  int old_pendown;
-
-  last_x = x;
-  last_y = y;
-
-  DBG("x=%d, y=%d, pressure=%d\n",x,y,pressure);
-
-  /* Transform and filter */
-  old_pendown = pen_down;
-  pen_down = pressure > pressure_tolerance;
-  if (touchscreen_filter(&x, &y, pen_down))
+void ucb1x00_packet(int x, int y, int pressure) {
+  static u8 state = 0;
+  int trigger;
+  
+  /* Quantize the pressure */
+  pressure = pressure > pressure_tolerance;
+  
+  /* Filter the sample, skipping one if necessary */
+  if (touchscreen_filter(&x, &y, pressure))
     return 1;
+  
+  /* Convert to screen coordinates */
   touchscreen_pentoscreen(&x, &y);
   
-  /* Show the cursor if 'showcursor' is on */
-  if (showcursor)
-    drivermessage(PGDM_CURSORVISIBLE,1,NULL);
+  /* What type of pointer event?
+   */
+  if (pressure) {
+    if (state)
+      trigger = TRIGGER_MOVE;
+    else
+      trigger = TRIGGER_DOWN;
+  }
+  else {
+    if (state)
+      trigger = TRIGGER_UP;
+    else
+      return 1;
+  }
   
-  if(pen_down && old_pendown) {
-    dispatch_pointing(TRIGGER_MOVE,x,y,1);
-  }
-  else if (pen_down && !old_pendown) {
-    dispatch_pointing(TRIGGER_DOWN,x,y,1);
-  }
-  else if (!pen_down && old_pendown) {
-    dispatch_pointing(TRIGGER_UP,x,y,1);
-  }
+  /* If we got this far, accept the new state and send the event */
+  state = (trigger != TRIGGER_UP);
+  dispatch_pointing(trigger,x,y,state);
 }
 
 
@@ -118,9 +119,6 @@ int ucb1x00_fd_activate(int active_fd) {
   /* No data yet - (shouldnt ever actaully call this)*/
   if (bytes_read == 0)
     return 0;
- 
-  /* reset the fd_init call counter so we dont issue pen up */
-  polls_since_data=0;
 
   ucb1x00_packet(ts_data.x, ts_data.y, ts_data.pressure);
 
@@ -129,29 +127,10 @@ int ucb1x00_fd_activate(int active_fd) {
 
 void ucb1x00_fd_init(int *n,fd_set *readfds,struct timeval *timeout) {
   if(fd<0) return;
-  
   if ((*n)<(fd+1))
     *n = fd+1;
- 
   if (fd>0)
     FD_SET(fd, readfds);
-
-  /* Set us up a polling time for detecting penups */
-  if (pen_down) {
-    timeout->tv_sec = 0;
-    if (timeout->tv_usec > 300)
-      timeout->tv_usec = 300;
-  }
-}
-
-void ucb1x00_poll(void) {
-  DBG("polls_since_data=%d\n",polls_since_data);
-
-  /* If it's been a while since we've gotten a sample, release the pen */
-  if (polls_since_data++ >= 3 && pen_down) {
-    ucb1x00_packet(last_x,last_y,0);
-    pen_down = 0;
-  }
 }
 
 void ucb1x00_message(u32 message, u32 param, u32 *ret) {
@@ -189,7 +168,6 @@ g_error ucb1x00_regfunc(struct inlib *i) {
   i->close = &ucb1x00_close;
   i->fd_activate = &ucb1x00_fd_activate;
   i->fd_init = &ucb1x00_fd_init;
-  i->poll = &ucb1x00_poll;
   i->message = &ucb1x00_message;
 
   return success;
