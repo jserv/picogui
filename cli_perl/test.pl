590aaed92fdeb52435129c6127039ad2eb0342bb
Micah Elizabeth Scott <micah@misc.name>
2002-07-18 14:48:51 +0000

    Made the hello world more hello-world-like

--- cli_perl/test.pl
+++ cli_perl/test.pl
@@ -4,9 +4,9 @@ $pg = new PicoGUI::Server;
 
 $app = $pg->Register($pg->mkString("Low-level cli_perl demo"),1,0);
 
-$w = $pg->mkWidget(2,5,$app);   # Create a button inside $app
+$w = $pg->mkWidget(2,1,$app);   # Create a label inside $app
 
-$pg->set($w,1<<3,2);                         # PG_WP_SIDE -> PG_S_TOP
+$pg->set($w,1<<11,2);                        # PG_WP_SIDE -> PG_S_ALL
 $pg->set($w,$pg->mkString("Hello World"),7); # PG_WP_TEXT -> Hello World
 
 $pg->Update;
