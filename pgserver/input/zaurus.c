8e50d64d5a055ee4e69590b033d7eba642ebe0ef
Micah Elizabeth Scott <micah@misc.name>
2002-08-23 12:26:29 +0000

    Created an input filter in the zaurus driver to handle the Z's keyboard

--- pgserver/input/zaurus.c
+++ pgserver/input/zaurus.c
@@ -1,4 +1,4 @@
-/* $Id: zaurus.c,v 1.7 2002/08/06 20:49:31 micahjd Exp $
+/* $Id: zaurus.c,v 1.8 2002/08/23 12:26:29 micahjd Exp $
  *
  * zaurus.c - Input driver for the Sharp Zaurus SL-5000. This includes a
  *            simple touchscreen driver, and some extras to handle sound
@@ -42,51 +42,59 @@ int zaurus_ts_fd;
 int zaurus_buz_fd;
 int zaurus_led_fd;
 
+struct infilter infilter_zaurus;
+handle zaurus_if;
+
 /******************************************** Implementations */
 
 g_error zaurus_init(void) {
-   g_error e;
+  g_error e;
+  
+  zaurus_ts_fd = open("/dev/sharp_ts",O_NONBLOCK);
+  if (zaurus_ts_fd <= 0)
+    return mkerror(PG_ERRT_IO, 74);
+  
+  /* Open some auxiliary devices... not really important if they fail */
+  zaurus_buz_fd = open("/dev/sharp_buz",O_WRONLY);
+  zaurus_led_fd = open("/dev/sharp_led",O_WRONLY);
 
-   zaurus_ts_fd = open("/dev/sharp_ts",O_NONBLOCK);
-   if (zaurus_ts_fd <= 0)
-     return mkerror(PG_ERRT_IO, 74);
-   
-   /* Open some auxiliary devices... not really important if they fail */
-   zaurus_buz_fd = open("/dev/sharp_buz",O_WRONLY);
-   zaurus_led_fd = open("/dev/sharp_led",O_WRONLY);
-
-   return success;
+  /* Insert our own input filter to handle oddities of the Zaurus keyboard */
+  e = infilter_insert(&infilter_list, &zaurus_if, -1, &infilter_zaurus);
+  errorcheck;
+  
+  return success;
 }
 
 void zaurus_close(void) {
-   close(zaurus_ts_fd);
-   close(zaurus_buz_fd);
-   close(zaurus_led_fd);
+  handle_delete(zaurus_if);
+  close(zaurus_ts_fd);
+  close(zaurus_buz_fd);
+  close(zaurus_led_fd);
 }
    
 void zaurus_fd_init(int *n,fd_set *readfds,struct timeval *timeout) {
-   if ((*n)<(zaurus_ts_fd+1))
-     *n = zaurus_ts_fd+1;
-   FD_SET(zaurus_ts_fd,readfds);
+  if ((*n)<(zaurus_ts_fd+1))
+    *n = zaurus_ts_fd+1;
+  FD_SET(zaurus_ts_fd,readfds);
 }
 
 int zaurus_ts_fd_activate(int fd) {
-   tsEvent ts;
-   
-   /* Read raw data from the driver */
-   if (fd!=zaurus_ts_fd)
-     return 0;
-   if (read(zaurus_ts_fd,&ts,sizeof(ts)) < sizeof(ts))
-     return 1;
-
-   infilter_send_touchscreen(ts.x,ts.y,ts.pressure,ts.pressure != 0);
-   
-   return 1;
+  tsEvent ts;
+  
+  /* Read raw data from the driver */
+  if (fd!=zaurus_ts_fd)
+    return 0;
+  if (read(zaurus_ts_fd,&ts,sizeof(ts)) < sizeof(ts))
+    return 1;
+  
+  infilter_send_touchscreen(ts.x,ts.y,ts.pressure,ts.pressure != 0);
+  
+  return 1;
 }
 
 void zaurus_message(u32 message, u32 param, u32 *ret) {
   switch (message) {
-
+    
   case PGDM_POWER:
     if (param <= PG_POWER_SLEEP) {
       int fd = open("/proc/sys/pm/suspend",O_WRONLY);
@@ -129,7 +137,45 @@ void zaurus_message(u32 message, u32 param, u32 *ret) {
     break;
   }
 }
-   
+
+/******************************************** Zaurus input filter */
+
+/* This input filter will handle zaurus-specific keyboard events, like
+ * powering off and adjusting the frontlight.
+ */
+void infilter_zaurus_handler(struct infilter *self, u32 trigger, union trigparam *param) {
+  static u32 last_cancel_press = 0;
+  u32 now;
+  union trigparam newtrig;
+
+  switch (param->kbd.key) {
+
+    /* Detect double-press of the cancel button as suspend */
+  case PGKEY_ESCAPE:
+    now = getticks();
+    if (now < last_cancel_press + 250)
+      driver_message(PGDM_POWER, PG_POWER_SLEEP);
+    else
+      last_cancel_press = now;
+    break;
+
+    /* Seems to be F6 is what happens when you press Fn-C, map that to CTRL-C */
+  case PGKEY_F6:
+    newtrig = *param;
+    newtrig.kbd.key = PGKEY_c;
+    newtrig.kbd.mods = PGMOD_CTRL;
+    infilter_send(self,PG_TRIGGER_KEY,&newtrig);
+    break;
+
+  }
+}
+
+struct infilter infilter_zaurus = {
+  accept_trigs: PG_TRIGGER_KEYDOWN,
+  absorb_trigs: 0,
+  handler: &infilter_zaurus_handler,
+};
+
 /******************************************** Driver registration */
 
 g_error zaurus_regfunc(struct inlib *i) {
