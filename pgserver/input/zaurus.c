bd4d3d557e356a3278a57e3a364adf6a6f2e37da
Micah Elizabeth Scott <micah@misc.name>
2002-08-23 12:33:14 +0000

    handle_free not handle_delete...

--- pgserver/input/zaurus.c
+++ pgserver/input/zaurus.c
@@ -1,4 +1,4 @@
-/* $Id: zaurus.c,v 1.9 2002/08/23 12:30:29 micahjd Exp $
+/* $Id: zaurus.c,v 1.10 2002/08/23 12:33:14 micahjd Exp $
  *
  * zaurus.c - Input driver for the Sharp Zaurus SL-5000. This includes a
  *            simple touchscreen driver, and some extras to handle sound
@@ -66,7 +66,7 @@ g_error zaurus_init(void) {
 }
 
 void zaurus_close(void) {
-  handle_delete(zaurus_if);
+  handle_free(zaurus_if);
   close(zaurus_ts_fd);
   close(zaurus_buz_fd);
   close(zaurus_led_fd);
