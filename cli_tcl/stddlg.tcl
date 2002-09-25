06ce93c4c22ed73034bb07d147638adf6f423b8b
espsoftware <espsoftware>
2002-09-25 19:19:38 +0000

    removed seperate dialog library

--- cli_tcl/stddlg.tcl
+++
@@ -1,7 +0,0 @@
-proc pgDialog { title } {
-	global pg_widget defaultparent defaltrship
-	set dlg [pgCreateWidget $pg_widget(dialogbox)]
-	pgSetText $dlg $title
-	set defaultparent $dlg
-	return $dlg
-}
