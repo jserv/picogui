466c949dde0e95a4727ee04e85510981a8f38391
espsoftware <espsoftware>
2002-09-26 18:47:46 +0000

    removed some old code

--- cli_tcl/eventtest.tcl
+++ cli_tcl/eventtest.tcl
@@ -2,11 +2,10 @@
 source picogui.tcl
 pgConnect localhost 0
 
-pgRegisterApp "Greetings" $pg_app(normal)
+pgRegisterApp "Event Test" $pg_app(normal)
 set b [pgNewWidget canvas]
 pgSetSide $b all
 pgSetWidget $b $pg_wp(triggermask) $pg_trigger(move)
 pgBind $b any {parray "event"}
-#pgBind $b $pg_we(activate) update
 pgUpdate
 pgEventLoop
