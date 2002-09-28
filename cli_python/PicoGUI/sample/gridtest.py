43e7b8f43dfe7cf93b279158f62f7d1a14f21626
Micah Elizabeth Scott <micah@misc.name>
2002-09-28 11:28:40 +0000

    Adding a little stress test demo I was using

---
+++ cli_python/PicoGUI/sample/gridtest.py
@@ -0,0 +1,11 @@
+import PicoGUI
+app = PicoGUI.Application('Pseudo-grid-test')
+box = app.addWidget('scrollbox')
+for y in range(0,20):
+    line = box.addWidget('box','inside')
+    line.side = 'top'
+    line.transparent = 1
+    for x in range(0,20):
+        w = line.addWidget('button','inside')
+        w.text = 'grid'
+app.run()
