35fdff934afa935f17700796781d3d01b9c062a3
espsoftware <espsoftware>
2002-09-28 23:10:29 +0000

    updated to work with the current new api

--- cli_tcl/buttontest.tcl
+++ cli_tcl/buttontest.tcl
@@ -3,11 +3,10 @@ source picogui.tcl
 pgui connect localhost 0
 
 set p [pgRegisterApp "Button Test" $pg_app(normal)]
-set l [pgCreateWidget button]
-pgAttach $l inside $p
-pgSetText $l "Hello World"
+set l [pgwidget create button]
+pgwidget attach  $l inside $p
+pgwidget set $l -text "Hello World" -side all
 pgSetFont $l [pgNewFont "" $pg_fstyle(bold) 24]
-pgSetSide $l all
 pgui update
 pgBind $l $pg_we(activate) {puts "hello"}
 pgBind any $pg_we(close) {puts "goodby"; exit}
