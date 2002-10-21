e42dda99c0e5dd671b19511ffe74aee1f835b4df
Micah Elizabeth Scott <micah@misc.name>
2002-10-21 17:57:19 +0000

    Renamed to "Burning cinder fury of crimson chaos fire", new wiggling and smoother blurring

--- apps/canvastst/chaos_fire.c
+++ apps/canvastst/chaos_fire.c
@@ -4,8 +4,9 @@
 pgcontext gc;
 const float pi = 3.1415926;
 
+
 float radius(float a, int t) {
-  return sin(a*5+t*0.03)*0.3+1;
+  return sin(a*5+t*0.03)*0.3+1 + sin(a*42+t*0.7)*0.05;
 }
 
 void animate(void) {
@@ -20,8 +21,8 @@ void animate(void) {
     pgSetLgop(gc,PG_LGOP_ALPHA);
     pgRect(gc,0,0,1000,1000);
     pgSetLgop(gc,PG_LGOP_NONE);
-    pgBlur(gc,0,0,1000,1000,2);
   }
+  pgBlur(gc,0,0,1000,1000,2);
 
   /* Draw our polar function defined above */
   pgSetColor(gc, 0xFF7344); 
@@ -40,7 +41,7 @@ int main(int argc,char **argv) {
   pgcontext bg;
 
   pgInit(argc,argv);
-  pgRegisterApp(PG_APP_NORMAL,"Funky canvas widget blurring fun",0);
+  pgRegisterApp(PG_APP_NORMAL,"Burning cinder fury of crimson chaos fire",0);
   pgNewWidget(PG_WIDGET_CANVAS,0,0);
 
   bg = pgNewCanvasContext(PGDEFAULT,PGFX_PERSISTENT);
