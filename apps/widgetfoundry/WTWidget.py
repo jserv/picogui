cb8ca39fca121b5f925862af3fdec89d93eb5f20
Micah Elizabeth Scott <micah@misc.name>
2002-11-15 07:38:21 +0000

    Adding GPL

--- apps/widgetfoundry/WTWidget.py
+++ apps/widgetfoundry/WTWidget.py
@@ -1,3 +1,25 @@
+# $Id: WTWidget.py,v 1.5 2002/11/15 07:38:21 micahjd Exp $
+#
+# WTWidget.py - Extensions to the PicoGUI widget class to support
+#               Widget Foundry's XWT exporting and property editing
+#
+# PicoGUI small and efficient client/server GUI
+# Copyright (C) 2000-2002 Micah Dowty <micahjd@users.sourceforge.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
 import PicoGUI, string, sys
 
 
