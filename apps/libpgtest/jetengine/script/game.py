0cc5d69aee2b6a50f49e62dd5c7ad13fabcf936d
Micah Elizabeth Scott <micah@misc.name>
2002-11-26 19:24:58 +0000

    forgot header on game.py

--- apps/libpgtest/jetengine/script/game.py
+++ apps/libpgtest/jetengine/script/game.py
@@ -1,3 +1,25 @@
+# $Id: game.py,v 1.24 2002/11/26 19:24:58 micahjd Exp $
+#
+# game.py - The main module imported by the game engine,
+#           and containing the thread() function it invokes.
+#
+# Copyright (C) 2002 Micah Dowty and David Trowbridge
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
 import PicoGUI, math, time
 import hud, input, mainmenu
 
