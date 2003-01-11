1d393beae7d1af35a43a5e33dc425939c8bd4696
brandon_smith <brandon_smith>
2003-01-11 03:00:25 +0000

    There, now it will compile

---
+++ apps/connectfour/ai10.h
@@ -0,0 +1,35 @@
+/*
+ * Filename:  ai10.h
+ * Author:    Brandon Smith
+ * Date:      November 24, 2002
+ * Purpose:   A decision tree based AI
+ *
+ * Copyright (C) 2002 Brandon Smith <lottabs2@yahoo.com> 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ *
+ */
+
+#ifndef __AI10_H__
+#define __AI10_H__
+
+#include "connectfour.h"
+
+
+
+void ai10(struct board *it);
+
+#endif /* __AI10_H__ */
