64a6957fafb83eb06b30e30a8d5d618f2576050d
lalo <lalo>
2003-04-07 14:35:26 +0000

    use basename to eat less space in the tab bar

--- apps/nifty/Nifty/FileBuffer.py
+++ apps/nifty/Nifty/FileBuffer.py
@@ -6,6 +6,7 @@ class FileBuffer(TextBuffer):
 
     def __init__(self, path=''):
         self.path = os.path.expanduser(os.path.expandvars(path))
+        self.basename = os.path.basename(self.path)
         self.changed = False
         if os.path.exists(self.path):
             f = file(self.path, 'r')
@@ -14,17 +15,17 @@ class FileBuffer(TextBuffer):
         else:
             print 'New file'
             text = ''
-        TextBuffer.__init__(self, name=path, text=text)
+        TextBuffer.__init__(self, name=self.basename, text=text)
 
     def notify_changed(self, ev):
         TextBuffer.notify_changed(self, ev)
         self.changed = True
-        self.change_name('*' + self.path)
+        self.change_name('*' + self.basename)
 
     def save(self):
         f = file(self.path, 'w')
         f.write(self.text)
         f.close()
-        print 'buffer %r saved' % self.name
+        print 'buffer %r saved' % self.basename
         self.changed = False
-        self.change_name(self.path)
+        self.change_name(self.basename)
