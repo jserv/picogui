d34f592bab12a902a2e686e84804c3f68f425cd7
lalo <lalo>
2003-04-07 14:32:05 +0000

    open scratch if nothing open, which is different from opening if nothing is given on command line

--- apps/nifty/nifty.py
+++ apps/nifty/nifty.py
@@ -3,11 +3,10 @@ import sys
 from Nifty import Frame, FileBuffer, ScratchBuffer
 
 frame = Frame("This here is my text editor. Is it not nifty? Worship the text editor!")
-if len(sys.argv) == 1:
+for name in sys.argv[1:]:
+    frame.open(FileBuffer(name))
+if len(frame._pages) == 0:
     frame.open(ScratchBuffer('__scratch__', 'This buffer is for experimenting with Nifty'))
-else:
-    for name in sys.argv[1:]:
-        frame.open(FileBuffer(name))
 
 sys.exit(frame.run())
 
