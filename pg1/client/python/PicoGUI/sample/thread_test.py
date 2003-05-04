e0de7128a72a50f297f19dde3abb88ee32f4c54a
lalo <lalo>
2003-05-04 18:14:00 +0000

    adding some threading debugging stuff

---
+++ pg1/client/python/PicoGUI/sample/thread_test.py
@@ -0,0 +1,26 @@
+import PicoGUI, threading, thread
+PicoGUI.server.debug_threads = 1
+app = PicoGUI.Application('Greetings')
+print 'main thread is', thread.get_ident()
+
+b = app.addWidget('Button')
+b.side = 'Bottom'
+b.text = 'poke'
+
+def thread_run():
+    app.run()
+
+t = threading.Thread(None,
+                     thread_run,
+                     None)
+t.start()
+
+l = app.addWidget('Label')
+l.side = 'All'
+l.text = 'Hello, World!'
+l.font = ':24:Bold'
+
+print 'hibernating'
+t.join()
+print 'bye'
+app.shutdown()
