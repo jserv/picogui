113028d8f1e05f21a34f87cf4d80ef66d96d2fdc
Micah Elizabeth Scott <micah@misc.name>
2003-05-04 12:06:36 +0000

    MiniSVN can do checkouts now :)

---
+++ pgbuild/src/build/lib/MiniDAV.py
@@ -0,0 +1,216 @@
+"""
+ A minimal read-only WebDAV client
+"""
+# 
+# PicoGUI Build System
+# Copyright (C) 2000-2003 Micah Dowty <micahjd@users.sourceforge.net>
+# 
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#  
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#  
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+# 
+
+from httplib import HTTPConnection
+from urlparse import *
+from xml.parsers import expat
+
+rev = "$Rev$"
+userAgent = "PicoGUI-MiniDAV/r" + rev.split()[1]
+
+
+class MiniDAVException(Exception):
+    pass
+
+class UnknownProtocol(MiniDAVException):
+    def __init__(self, name):
+        self.args = name,
+        self.name = name
+
+class ErrorResponse(MiniDAVException):
+    def __init__(self, resp):
+        self.args = "%s %s" % (resp.status, resp.reason),
+        self.status = resp.status
+        self.reason = resp.reason
+
+class InvalidDavResponse(MiniDAVException):
+    pass
+
+
+class DavPropertyParser:
+    """Utility to parse the XML responses from a PROPFIND request"""
+    def __init__(self):
+        self.elementStack = []
+        self.responses = {}
+        self.currentResponse = None
+        self.currentProperty = None
+    
+    def __startElementHandler(self, name, attrs):
+        self.elementStack.append(name)
+        if name == 'DAV::response':
+            self.currentResponse = {}
+
+        # Consider a tag immediately inside a <D:prop> to be a property
+        try:
+            if self.elementStack[-2] == 'DAV::prop':
+                self.currentProperty = self.elementStack[-1]
+        except IndexError:
+            pass
+
+        # If we get a tag inside a property tag, it might be worth saving. If we also
+        # get data, as in the case of <D:href>, the data will overwrite this. But, if we only
+        # have a tag this will end up storing the tag name as the property value. This is a
+        # hackish but painless way to make tags like <D:collection/> visible.
+        try:
+            if self.elementStack[-3] == 'DAV::prop':
+                self.currentResponse[self.currentProperty] = self.elementStack[-1]
+        except IndexError:
+            pass
+        
+            
+    def __endElementHandler(self, name):
+        if self.elementStack.pop() != name:
+            raise InvalidDavResponse()
+
+        # If we just left a tag immediately inside the <D:prop>, clear the current property
+        try:
+            if self.elementStack[-1] == 'DAV::prop':
+                self.currentProperty = None
+        except IndexError:
+            pass
+
+    def __characterDataHandler(self, data):
+        # The href tag sent immediately inside a new response tag will give the response
+        # its name in our dictionary
+        if self.elementStack[-1] == 'DAV::href' and self.elementStack[-2] == 'DAV::response':
+            self.responses[data] = self.currentResponse
+
+        # If we think we're inside a property, stow this data
+        if self.currentProperty:
+            self.currentResponse[self.currentProperty] = data
+
+    def parse(self, data):
+        Parser = expat.ParserCreate("UTF-8", ":")        
+        Parser.StartElementHandler = self.__startElementHandler
+        Parser.EndElementHandler = self.__endElementHandler
+        Parser.CharacterDataHandler = self.__characterDataHandler
+        Parser.Parse(data)
+
+        
+class DavObject:
+    """Represent an object on a WebDAV server"""
+    def __init__(self, url):
+        parsedURL = urlparse(url)
+        if parsedURL[0] != 'http':
+            raise UnknownProtocol(parsedURL[0])
+        self.url = url
+        self.server = parsedURL[1]
+        self.path = parsedURL[2]
+
+    def read(self):
+        """Read the contents of the entire object, returning it"""
+        global userAgent
+        conn = HTTPConnection(self.server)
+        conn.request("GET", self.path, None,
+                     { 'User-Agent': userAgent,
+                       })       
+        resp = conn.getresponse()
+        if resp.status != 200:
+            raise ErrorResponse(resp)
+        data = resp.read()
+        conn.close()
+        return data
+        
+    def propfind(self):
+        """Read the object's properties, storing them. This is called
+           automatically once if property values are needed, but may
+           be called again to invalidate the locally cached properties.
+        """
+        global userAgent
+        conn = HTTPConnection(self.server)
+        conn.request("PROPFIND", self.path, None,
+                     { 'Depth': 1,
+                       'User-Agent': userAgent,
+                       })       
+        resp = conn.getresponse()
+        if resp.status != 207:
+            raise ErrorResponse(resp)
+        parser = DavPropertyParser()
+        parser.parse(resp.read())
+        conn.close()
+
+        # We now have a dictionary of the responses, keyed by the href.
+        # If the URL we were given didn't have a trailing slash even though
+        # it was a collection, it won't match exactly.
+        self.properties = None
+        for response in parser.responses.keys():
+            if response == self.path or response == self.path + '/':
+                # We can tell based on whether this response has a trailing slash,
+                # if this object is a collection or a file.
+                if response[-1] == '/':
+                    self.type = 'collection'
+                else:
+                    self.type = 'file'
+
+                self.properties = parser.responses[response]
+                del parser.responses[response]
+        if not self.properties:
+            raise InvalidDavResponse()
+
+        # Now that the response for this object has been safely tucked away,
+        # all the objects left in the list, if any, should be subdirectories.
+        # Convert each one of them to a DavObject, and store the properties
+        # since we already have them. Note that this leaves the children's
+        # children properties undefined.
+        self.children = []
+        for response in parser.responses.keys():
+            obj = DavObject(urlunparse(('http', self.server, response, '', '', '')))
+            obj.properties = parser.responses[response]
+
+            # As above, we can tell from the response whether the child is a collection
+            # or not. If not, go ahead and give it an empty children list.
+            if response[-1] == '/':
+                obj.type = 'collection'
+            else:
+                obj.type = 'file'
+                obj.children = []
+
+            self.children.append(obj)
+
+    def getChildren(self):
+        if not hasattr(self, "children"):
+            self.propfind()
+        return self.children
+
+    def getProperties(self):
+        if not hasattr(self, "properties"):
+            self.propfind()
+        return self.properties
+
+    def getType(self):
+        if not hasattr(self, "type"):
+            self.propfind()
+        return self.type
+
+
+if __name__ == '__main__':
+    import sys
+    obj = DavObject(sys.argv[1])
+    print "\nURL: %s" % obj.url
+
+    print "\nChildren:"
+    for child in obj.getChildren():
+        print "\t%s" % child.path
+
+    print "\nProperties:"
+    for property in obj.getProperties():
+        print "\t%s = %s" % (property, obj.getProperties()[property])
--- pgbuild/src/build/lib/MiniSVN.py
+++ pgbuild/src/build/lib/MiniSVN.py
@@ -1,7 +1,7 @@
 """
  MiniSVN.py - A minimalist Subversion client capable of doing
               multithreaded atomic checkouts, using only the
-              Python standard library.
+              Python standard library and MiniDAV.
 """
 # 
 # PicoGUI Build System
@@ -22,185 +22,103 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 # 
 
-import httplib
-import urlparse
-import xml.parsers.expat
+from urlparse import *
+import threading
+from MiniDAV import DavObject
+import os, re
 
-rev = "$Rev$"
-userAgent = "PicoGUI-MiniSVN/r" + rev.split()[1]
+class DownloaderThread(threading.Thread):
+    def __init__(self, queue, queueSem):
+        self.queue = queue
+        self.queueSem = queueSem
+        threading.Thread.__init__(self)
 
+    def run(self):
+        """Thread-safe queue management, to feed items into download()
+           and enqueue children.
+           """
+        while True:
+            self.queueSem.acquire()
+            if len(self.queue) <= 0:
+                self.queueSem.release()
+                break
+            queueItem = self.queue.pop()
+            self.queueSem.release()
+            newItems = self.download(queueItem)
+            if newItems:
+                self.queueSem.acquire()            
+                for item in newItems:
+                    self.queue.append(item)
+                self.queueSem.release()
 
-class MiniSvnException(Exception):
-    pass
+    def download(self, item):
+        """Download an item from the queue, returning a list of
+           more items to add to the queue.
+           """
+        (object, destination) = item
 
-class UnknownProtocol(MiniSvnException):
-    def __init__(self, name):
-        self.args = name,
-        self.name = name
+        if object.getType() == 'collection':
+            # This object is a directory- recursively create it
+            os.makedirs(destination)
+            print " d %s" % destination
 
-class ErrorResponse(MiniSvnException):
-    def __init__(self, resp):
-        self.args = "%s %s" % (resp.status, resp.reason),
-        self.status = resp.status
-        self.reason = resp.reason
-
-class InvalidDavResponse(MiniSvnException):
-    pass
-
-
-class DavPropertyParser:
-    """Utility to parse the XML responses from a PROPFIND request"""
-    def __init__(self):
-        self.elementStack = []
-        self.responses = {}
-        self.currentResponse = None
-        self.currentProperty = None
-    
-    def __startElementHandler(self, name, attrs):
-        self.elementStack.append(name)
-        if name == 'DAV::response':
-            self.currentResponse = {}
-
-        # Consider a tag immediately inside a <D:prop> to be a property
-        try:
-            if self.elementStack[-2] == 'DAV::prop':
-                self.currentProperty = self.elementStack[-1]
-        except IndexError:
-            pass
-
-        # If we get a tag inside a property tag, it might be worth saving. If we also
-        # get data, as in the case of <D:href>, the data will overwrite this. But, if we only
-        # have a tag this will end up storing the tag name as the property value. This is a
-        # hackish but painless way to make tags like <D:collection/> visible.
-        try:
-            if self.elementStack[-3] == 'DAV::prop':
-                self.currentResponse[self.currentProperty] = self.elementStack[-1]
-        except IndexError:
-            pass
+        elif object.getType() == 'file':
+            # This object is a file- download it, creating the directory if necessary
+            try:
+                f = open(destination, "wb")
+            except IOError:
+                os.makedirs(os.path.join(os.path.split(destination)[:-1])[0])
+                f = open(destination, "wb")                
+            f.write(object.read())
+            f.close()
+            print "-> %s" % destination
         
-            
-    def __endElementHandler(self, name):
-        if self.elementStack.pop() != name:
-            raise InvalidDavResponse()
+        # Get our children ready to queue
+        enqueue = []
+        for child in object.getChildren():
+            # Find the part of the child's URL that was (presumably)
+            # appended to this object's URL and append it to the destination
+            suffix = child.url[len(object.url):].replace("/","")
+            enqueue.append((child, os.path.join(destination, suffix) ))
+        return enqueue
 
-        # If we just left a tag immediately inside the <D:prop>, clear the current property
-        try:
-            if self.elementStack[-1] == 'DAV::prop':
-                self.currentProperty = None
-        except IndexError:
-            pass
 
-    def __characterDataHandler(self, data):
-        # The href tag sent immediately inside a new response tag will give the response
-        # its name in our dictionary
-        if self.elementStack[-1] == 'DAV::href' and self.elementStack[-2] == 'DAV::response':
-            self.responses[data] = self.currentResponse
-
-        # If we think we're inside a property, stow this data
-        if self.currentProperty:
-            self.currentResponse[self.currentProperty] = data
-
-    def parse(self, data):
-        Parser = xml.parsers.expat.ParserCreate("UTF-8", ":")        
-        Parser.StartElementHandler = self.__startElementHandler
-        Parser.EndElementHandler = self.__endElementHandler
-        Parser.CharacterDataHandler = self.__characterDataHandler
-        Parser.Parse(data)
-
-        
-class DavObject:
-    """Represent an object on a WebDAV server"""
+class SVNRepository(DavObject):
     def __init__(self, url):
-        parsedURL = urlparse.urlparse(url)
-        if parsedURL[0] != 'http':
-            raise UnknownProtocol(parsedURL[0])
-        self.url = url
-        self.server = parsedURL[1]
-        self.path = parsedURL[2]
+        # The caller may pass a URL of the form "/repository/!svn/bc/<revision>/path"
+        # to check out a particular revision. If they haven't already specified a
+        # special path like this, find the latest revision number and use that.
+        # This part is what makes checkouts atomic.
+        if url.find("!svn") < 0:
+            # This part comes from guesstimation and looking at apache logs...
+            # I couldn't find any docs on the WebDAV URLs used by Subversion, and
+            # this was faster than digging through the source :)
+            reposPath = DavObject(url).getProperties()['DAV::checked-in']
+            reposPath = reposPath.replace("/!svn/ver/", "/!svn/bc/")
+            parsed = urlparse(url)
+            url = urlunparse((parsed[0], parsed[1], reposPath, '', '', ''))
+        DavObject.__init__(self, url)
 
-    def read(self):
-        """Read the contents of the entire object, returning it"""
-        global userAgent
-        conn = httplib.HTTPConnection(self.server)
-        conn.request("GET", self.path, None,
-                     { 'User-Agent': userAgent,
-                       })       
-        resp = conn.getresponse()
-        if resp.status != 200:
-            raise ErrorResponse(resp)
-        data = resp.read()
-        conn.close()
-        return data
-        
-    def propfind(self):
-        """Read the object's properties, storing them. This is called
-           automatically once if property values are needed, but may
-           be called again to invalidate the locally cached properties.
-        """
-        global userAgent
-        conn = httplib.HTTPConnection(self.server)
-        conn.request("PROPFIND", self.path, None,
-                     { 'Depth': 1,
-                       'User-Agent': userAgent,
-                       })       
-        resp = conn.getresponse()
-        if resp.status != 207:
-            raise ErrorResponse(resp)
-        parser = DavPropertyParser()
-        parser.parse(resp.read())
-        conn.close()
+    def download(self, destination, numThreads=10):
 
-        # We now have a dictionary of the responses, keyed by the href.
-        # If the URL we were given didn't have a trailing slash even though
-        # it was a collection, it won't match exactly.
-        self.properties = None
-        for response in parser.responses.keys():
-            if response == self.path or response == self.path + '/':
-                self.properties = parser.responses[response]
-                del parser.responses[response]
-        if not self.properties:
-            raise InvalidDavResponse()
+        # If this repository consists of just one file, go ahead and join that to
+        # the destination path.
+        if len(self.getChildren()) == 0:
+            suffix = re.search("/([^/]+)$", self.path).group(1)
+            destination = os.path.join(destination, suffix)
 
-        # Now that the response for this object has been safely tucked away,
-        # all the objects left in the list, if any, should be subdirectories.
-        # Convert each one of them to a DavObject, and store the properties
-        # since we already have them. Note that this leaves the children's
-        # children properties undefined.
-        self.children = []
-        for response in parser.responses.keys():
-            obj = DavObject(urlparse.urlunparse(('http', self.server, response, '', '', '')))
-            obj.properties = parser.responses[response]
-
-            # If the child isn't a collection, give it an empty children list to
-            # save us from having to call propfind() to figure that out later.
-            if hasattr(obj.properties, 'DAV::resourcetype'):
-                if obj.properties['DAV::resourcetype'] != 'DAV::collection':
-                    obj.children = []
-            else:
-                obj.children = []
-
-            self.children.append(obj)
-
-    def getChildren(self):
-        if not hasattr(self, "children"):
-            self.propfind()
-        return self.children
-
-    def getProperties(self):
-        if not hasattr(self, "properties"):
-            self.propfind()
-        return self.properties
+        queue = [(self, destination)]
+        queueSem = threading.Semaphore()
+        threads = []
+        for i in xrange(numThreads):
+            thread = DownloaderThread(queue, queueSem)
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
 
 
 if __name__ == '__main__':
     import sys
-    obj = DavObject(sys.argv[1])
-    print "\nURL: %s" % obj.url
-
-    print "\nChildren:"
-    for child in obj.getChildren():
-        print "\t%s" % child.path
-
-    print "\nProperties:"
-    for property in obj.getProperties():
-        print "\t%s = %s" % (property, obj.getProperties()[property])
+    repo = SVNRepository(sys.argv[1])
+    repo.download("tmp")
