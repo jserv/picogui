93389e7e35efb9e9bc00e34cfd531b224ed4a7d7
bornet <bornet>
2003-05-05 09:30:28 +0000

    Put back the autoconf/automake stuff for apps.

---
+++ pg1/apps/acconfig.h
@@ -0,0 +1,10 @@
+/* $Id: acconfig.h,v 1.3 2002/09/20 12:01:35 cgrigis Exp $ */
+
+/* If set, we are compiling for PocketBee */
+#undef POCKETBEE
+
+/* If set, use the PocketBee Process Manager */
+#undef USE_PM
+
+/* To enable debug */
+#undef DEBUG
