1b4a272f7cda35ae6fc4bd193871ace9cdcff679
Micah Elizabeth Scott <micah@misc.name>
2003-05-09 23:26:58 +0000

    A little organization for the pgbuild configuration system

---
+++ pgbuild/src/build/Package.py
@@ -0,0 +1,33 @@
+"""
+ Package.py - Representation of a source package that can be automatically
+              downloaded, and that will participate in the
+              configure/build process.
+"""
+# 
+# PicoGUI Build System
+# Copyright (C) 2000-2003 Micah Dowty <micahjd@users.sourceforge.net>
+# 
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#  
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#  
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+# 
+
+def Package:
+    """The package object should be constructed in the package list config file"""
+    def __init__(name, repositories):
+        self.name = name
+        self.repository = repository
+
+### The End ###
+        
+    
