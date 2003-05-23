36f3ef7a803aa319c3457ccfe6bfd2610b18eeff
Micah Elizabeth Scott <micah@misc.name>
2003-05-23 06:08:54 +0000

    Fixing an incorrect comment

--- tools/irc/announceClient.py
+++ tools/irc/announceClient.py
@@ -7,8 +7,6 @@
 from twisted.internet import reactor, protocol
 
 class AnnounceClient(protocol.Protocol):
-    """Once connected, send a message, then print the result."""
-    
     def connectionMade(self):
         import sys
         self.transport.write("Announce %s %s\r\n" % (sys.argv[2], " ".join(sys.argv[3:])))
