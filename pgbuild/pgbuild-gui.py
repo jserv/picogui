723890c654044d29fb3585f112b15165ab60c2ce
Micah Elizabeth Scott <micah@misc.name>
2003-05-29 22:20:11 +0000

    Adding doc strings to entry point scripts

--- pgbuild/pgbuild-gui.py
+++ pgbuild/pgbuild-gui.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#
-# Yet another entry point, but automatically prepends --gui to all the given
-# command line options, causing a graphical interface to be opened. This is for
-# the convenience of point-and-click operating systems like Windows and MacOS.
-#
+"""
+Yet another entry point, but automatically prepends --gui to all the given
+command line options, causing a graphical interface to be opened. This is for
+the convenience of pointy-clicky operating systems like Windows and MacOS.
+"""
 import pgbuild
 pgbuild.main(['--ui=auto'])
