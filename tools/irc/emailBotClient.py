abd7c4df84183247eed8d8daa93868b771eefda2
Micah Elizabeth Scott <micah@misc.name>
2003-06-01 14:43:12 +0000

    Adding an email client for announceBot

---
+++ tools/irc/emailBotClient.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+ A client for the announceBot that accepts email messages from stdin.
+ This is meant to be used from a .forward file.
+ The message should have a subject of the form "Announce <channel>".
+ Every non-blank line of the body will be sent as a message to the bot.
+"""
+from twisted.internet import reactor, protocol
+import sys, email
+
+socketName = "/tmp/announceBot.socket"
+
+class AnnounceClient(protocol.Protocol):
+    def connectionMade(self):
+        import sys
+        mailMsg  = email.message_from_file(sys.stdin)
+        subjectFields = mailMsg['Subject'].split(" ")
+        messages = mailMsg.get_payload().split("\n")
+        if subjectFields[0] == "Announce":
+            for line in messages:
+	    	line = line.strip()
+		if len(line) > 0:
+	            self.transport.write("%s %s %s\r\n" %
+                                         ("Announce", subjectFields[1], line))
+        self.transport.loseConnection()
+    
+    def connectionLost(self, reason):
+        from twisted.internet import reactor
+        reactor.stop()
+
+class AnnounceClientFactory(protocol.ClientFactory):
+    protocol = AnnounceClient
+
+    def clientConnectionFailed(self, connector, reason):
+        reactor.stop()
+    
+    def clientConnectionLost(self, connector, reason):
+        reactor.stop()
+
+if __name__ == '__main__':
+    import sys
+    f = AnnounceClientFactory()
+    reactor.connectUNIX(socketName, f)
+    reactor.run()
