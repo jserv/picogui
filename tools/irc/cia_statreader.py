5bd121d12c26dc6ccc70ed80bf9f83988a3a8c4c
Micah Elizabeth Scott <micah@misc.name>
2003-06-02 10:34:42 +0000

    Quick and dirty separation of the cia_stats CGI into a CGI frontend and a stat reading backend

---
+++ tools/irc/cia_statreader.py
@@ -0,0 +1,51 @@
+#
+# Simple python module for processing CIA stats
+#
+
+import os
+
+statDir = "/home/commits/stats"
+channelFile = "/home/commits/channels.list"
+
+# List out the subdirs explicitly so we can set the order-
+# the first one here is used as the sort key, and for definatively
+# listing the available projects.
+statSubdirs = ('forever', 'monthly', 'weekly', 'end-of-day', 'daily')
+
+# Mapping between subdirectory names and table headings
+statHeadings = {
+    'project': 'project',
+    'forever': 'forever',
+    'monthly': 'this month',
+    'weekly': 'this week',
+    'end-of-day': 'yesterday',
+    'daily': 'today',
+    }
+
+def readStats():
+    """Stats are automatically read into the module on import,
+       this can be called separately to refresh them.
+       """
+    global projects, channels, projectCounts
+    
+    projects = os.listdir(os.path.join(statDir, statSubdirs[0]))
+    channels = open(channelFile).read().strip().split("\n")
+
+    projectCounts = {}
+    for project in projects:
+        # Build a map of counts indexed by subdirectory
+        counts = {}
+        for subdir in statSubdirs:
+            try:
+                counts[subdir] = int(open(os.path.join(statDir, subdir, project)).read().strip())
+            except IOError:
+                counts[subdir] = 0
+        projectCounts[project] = counts
+
+    # Sort the project list by the 'forever' count, descending
+    def countSort(a,b):
+        return cmp(projectCounts[b][statSubdirs[0]],
+                   projectCounts[a][statSubdirs[0]])
+    projects.sort(countSort)
+
+readStats()
--- tools/irc/cia_stats.cgi
+++ tools/irc/cia_stats.cgi
@@ -5,44 +5,7 @@
 #
 
 import os
-
-statDir = "/home/commits/stats"
-channelFile = "/home/commits/channels.list"
-
-# List out the subdirs explicitly so we can set the order-
-# the first one here is used as the sort key, and for definatively
-# listing the available projects.
-statSubdirs = ('forever', 'monthly', 'weekly', 'end-of-day', 'daily')
-
-# Mapping between subdirectory names and table headings
-statHeadings = {
-    'project': 'project',
-    'forever': 'forever',
-    'monthly': 'this month',
-    'weekly': 'this week',
-    'end-of-day': 'yesterday',
-    'daily': 'today',
-    }
-
-projects = os.listdir(os.path.join(statDir, statSubdirs[0]))
-channels = open(channelFile).read().strip().split("\n")
-
-projectCounts = {}
-for project in projects:
-    # Build a map of counts indexed by subdirectory
-    counts = {}
-    for subdir in statSubdirs:
-        try:
-            counts[subdir] = int(open(os.path.join(statDir, subdir, project)).read().strip())
-        except IOError:
-            counts[subdir] = 0
-    projectCounts[project] = counts
-
-# Sort the project list by the 'forever' count, descending
-def countSort(a,b):
-    return cmp(projectCounts[b][statSubdirs[0]],
-               projectCounts[a][statSubdirs[0]])
-projects.sort(countSort)
+from cia_statreader import *
 
 # Page heading
 print """Content-type: text/html
