e7cf136ada5853ec8d102b1aa3b5f0b094488430
Micah Elizabeth Scott <micah@misc.name>
2003-06-28 22:50:58 +0000

    Brought a big chunk of code from SCons' Script module into PGBuild's Build module. The exception handling and option-related code will need redoing, but pgbuild now at least gives a reasonable error message as to why it won't compile anything yet.

--- pgbuild/src/build-dev/PGBuild/Build.py
+++ pgbuild/src/build-dev/PGBuild/Build.py
@@ -3,7 +3,8 @@
 Manages the actual build process using SCons.
 This includes facilities to construct an SCons environment using the
 configuration database, to set up other SCons parameters, execute
-SConscripts, and build targets.
+SConscripts, and build targets. Parts of this code have been adapted
+from SCons' Script module.
 """
 # 
 # PicoGUI Build System
@@ -27,6 +28,101 @@ _svn_id = "$Id$"
 
 # Acceptable names for an SCons script, in order of preference
 scriptNames = ['SConscript', 'Sconscript', 'sconscript']
+
+import SCons.Taskmaster
+import sys
+
+# FIXME: move these to invocation options
+keep_going_on_error = False
+ignore_errors = False
+exit_status = 0
+
+class BuildTask(SCons.Taskmaster.Task):
+    """An SCons build task."""
+    def display(self, message):
+        display('scons: ' + message)
+
+    def execute(self):
+        target = self.targets[0]
+        if target.get_state() == SCons.Node.up_to_date:
+            if self.top and target.has_builder():
+                display('scons: "%s" is up to date.' % str(target))
+        elif target.has_builder() and not hasattr(target.builder, 'status'):
+            if print_time:
+                start_time = time.time()
+            SCons.Taskmaster.Task.execute(self)
+            if print_time:
+                finish_time = time.time()
+                global command_time
+                command_time = command_time+finish_time-start_time
+                print "Command execution time: %f seconds"%(finish_time-start_time)
+
+    def do_failed(self, status=2):
+        global exit_status
+        if ignore_errors:
+            SCons.Taskmaster.Task.executed(self)
+        elif keep_going_on_error:
+            SCons.Taskmaster.Task.fail_continue(self)
+            exit_status = status
+        else:
+            SCons.Taskmaster.Task.fail_stop(self)
+            exit_status = status
+            
+    def executed(self):
+        t = self.targets[0]
+        if self.top and not t.has_builder() and not t.side_effect:
+            if not t.exists():
+                sys.stderr.write("scons: *** Do not know how to make target `%s'." % t)
+                if not keep_going_on_error:
+                    sys.stderr.write("  Stop.")
+                sys.stderr.write("\n")
+                self.do_failed()
+            else:
+                print "scons: Nothing to be done for `%s'." % t
+                SCons.Taskmaster.Task.executed(self)
+        else:
+            SCons.Taskmaster.Task.executed(self)
+
+        # print the tree here instead of in execute() because
+        # this method is serialized, but execute isn't:
+        if print_tree and self.top:
+            print
+            print SCons.Util.render_tree(self.targets[0], get_all_children)
+        if print_dtree and self.top:
+            print
+            print SCons.Util.render_tree(self.targets[0], get_derived_children)
+        if print_includes and self.top:
+            t = self.targets[0]
+            tree = t.render_include_tree()
+            if tree:
+                print
+                print tree
+
+    def failed(self):
+        e = sys.exc_value
+        status = 2
+        if sys.exc_type == SCons.Errors.BuildError:
+            sys.stderr.write("scons: *** [%s] %s\n" % (e.node, e.errstr))
+            if e.errstr == 'Exception':
+                traceback.print_exception(e.args[0], e.args[1], e.args[2])
+        elif sys.exc_type == SCons.Errors.UserError:
+            # We aren't being called out of a user frame, so
+            # don't try to walk the stack, just print the error.
+            sys.stderr.write("\nscons: *** %s\n" % e)
+        elif sys.exc_type == SCons.Errors.StopError:
+            s = str(e)
+            if not keep_going_on_error:
+                s = s + '  Stop.'
+            sys.stderr.write("scons: *** %s\n" % s)
+        elif sys.exc_type == SCons.Errors.ExplicitExit:
+            status = e.status
+            sys.stderr.write("scons: *** [%s] Explicit exit, status %s\n" % (e.node, e.status))
+        else:
+            if e is None:
+                e = sys.exc_type
+            sys.stderr.write("scons: *** %s\n" % e)
+
+        self.do_failed(status)
 
 def loadScript(name, progress):
     """Load one SCons script"""
@@ -59,27 +155,32 @@ class System(object):
        """
 
     def __init__(self, config):
-        self.config = config
-
-        # Initialize SCons
         import SCons.Node
         import SCons.Defaults
         import SCons.Script
+        self.config = config
         SCons.Node.FS.default_fs.set_toplevel_dir(config.eval('bootstrap/path[@name="root"]/text()'))
         self.defaultEnv = SCons.Defaults._default_env = Environment(config)
+        self.processInvocationOptions()
 
-        # Set default targets
-        self.targets = SCons.Script.SConscript.default_targets
-
-        # If targets were specified on the command line, run those instead
-        invocationTargets = config.listEval('invocation/target/text()')
-        if invocationTargets:
-            self.targets = invocationTargets
+    def processInvocationOptions(self):
+        # Default task is to build targets
+        self.task_class = BuildTask
+        
+        self.invocationTargets = self.config.listEval('invocation/target/text()')
 
     def run(self, progress):
         import SCons.Node
-        import SCons.Taskmaster
         import SCons.Job
+
+        # Set default targets
+        self.defaultTargets = SCons.Script.SConscript.default_targets
+
+        # Determine a final list of targets to build
+        if self.invocationTargets:
+            self.targets = self.invocationTargets
+        else:
+            self.targets = self.defaultTargets
         
         # Convert our list of targets to nodes. The targets may be originally specified
         # as nodes, filenames, or aliases.
@@ -111,7 +212,7 @@ class System(object):
             raise PGBuild.Errors.ExternalError("No targets to build")
 
         # Create a taskmaster using a list of target nodes
-        taskmaster = SCons.Taskmaster.Taskmaster(nodes)
+        taskmaster = SCons.Taskmaster.Taskmaster(nodes, self.task_class)
 
         # This sets up several job threads to run tasks concurrently
         jobs = SCons.Job.Jobs(self.config.intEval("invocation/option[@name='numJobs']/text()"), taskmaster)
