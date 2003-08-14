f716992d7005762f5c000016d3120c09dbf08cd5
bornet <bornet>
2003-08-14 12:26:41 +0000

    Description and copyright correction.

--- pg1/server/input/btkey.c
+++ pg1/server/input/btkey.c
@@ -1,27 +1,32 @@
 /*
-   This file is part of PocketBee.
-  
-   Copyright (C) 2003 SMARTDATA (SA), Alain Paschoud 
-
-   File written by Alain Paschoud <alain.paschoud@smartdata.ch>, 2003
-
-   PocketBee is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-  
-   PocketBee is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-  
-   You should have received a copy of the GNU General Public License
-   along with PocketBee; see the file COPYING.  If not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-
-   $Id$
-*/
+ * Copyright (C) 2003 SMARTDATA (SA), Alain Paschoud 
+ *
+ * File written by Alain Paschoud <alain.paschoud@smartdata.ch>, 2003
+ *
+ * See README.btkey for more information
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Author:
+ *   Alain Paschoud <alain.paschoud@smartdata.ch>
+ * 
+ * Contributors:
+ *   Olivier Bornet <Olivier.Bornet@smardata.ch>
+ *
+ * $Id$
+ */
 
 
 #include <termios.h>
